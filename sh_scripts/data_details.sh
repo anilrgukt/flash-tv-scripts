e3edb20cbe74df71541e90e528ec6331d9f475eb
anilrgukt <anil.rgukt@gmail.com>
2023-02-15 14:48:19 -0600

    added video capture sh script

--- sh_scripts/data_details.sh
+++ sh_scripts/data_details.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Enter the 6 digit family ID below (3 digit family ID, and 3 digit Jetson device ID)
-famId=114009
-usrName=flashsys009
-savePath=/home/$usrName/data/114009_data
+famId=114089
+usrName=akv
+savePath=/home/$usrName/data/114089_data
 
 
