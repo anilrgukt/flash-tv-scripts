ee56e96c7610bccde7463d06b3167027305fbd1c
anilrgukt <anil.rgukt@gmail.com>
2023-02-16 12:03:01 -0600

    modified sh scripts

--- flash_periodic_restart.sh
+++ flash_periodic_restart.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 export famId=XXX
 export usrName=flashsysXXX
 
--- sh_scripts/data_details.sh
+++ sh_scripts/data_details.sh
@@ -3,6 +3,6 @@
 # Enter the 6 digit family ID below (3 digit family ID, and 3 digit Jetson device ID)
 famId=114089
 usrName=akv
-savePath=/home/$usrName/data/114089_data
+savePath=/home/$usrName/data/"${famId}_data"
 
 
