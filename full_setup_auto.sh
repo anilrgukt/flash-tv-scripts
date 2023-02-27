5ea4eac9c732d3a43a50a86b408ca8f4326cd6f7
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-27 15:16:52 -0600

    -x optional now

--- full_setup_auto.sh
+++ full_setup_auto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bash -x ~/flash-tv-scripts/rename_ids.sh
-bash -x ~/flash-tv-scripts/FLASH_filesetup.sh
-bash -x ~/flash-tv-scripts/install_scripts/full_install_auto.sh
+bash ~/flash-tv-scripts/rename_ids.sh
+bash ~/flash-tv-scripts/FLASH_filesetup.sh
+bash ~/flash-tv-scripts/install_scripts/full_install_auto.sh
