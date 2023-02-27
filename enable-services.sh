e2867227b6ce77924435d6e10a67742576472021
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-27 15:15:23 -0600

    Update enable-services.sh

--- enable-services.sh
+++ enable-services.sh
@@ -1,7 +1,3 @@
-sudo cp ~/flash-tv-scripts/flash-run-on-boot.service /etc/systemd/system
-sudo cp ~/flash-tv-scripts/flash-periodic-restart.service /etc/systemd/system
-sudo cp ~/flash-tv-scripts/homeassistant-run-on-boot.service /etc/systemd/system
-sudo systemctl daemon-reload
 sudo systemctl enable flash-periodic-restart.service
 sudo systemctl enable flash-run-on-boot.service
 sudo systemctl enable homeassistant-run-on-boot.service
