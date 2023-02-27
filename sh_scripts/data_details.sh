e5eb977a2e4257f1de7389729f725d830b73a201
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-27 12:23:40 -0600

    Update data_details.sh

--- sh_scripts/data_details.sh
+++ sh_scripts/data_details.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Enter the 6 digit family ID below (3 digit family ID, and 3 digit Jetson device ID)
-famId=114XXX
+famId=123XXX
 usrName=flashsysXXX
 savePath=/home/$usrName/data/"${famId}_data"
 
