04bc8922b1e9ed7759d8ea7555faced0bb6864b1
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-27 15:54:45 -0600

    Update FLASH_filesetup.sh

--- FLASH_filesetup.sh
+++ FLASH_filesetup.sh
@@ -6,7 +6,7 @@ sudo cp ~/flash-tv-scripts/flash-periodic-restart.service /etc/systemd/system
 sudo cp ~/flash-tv-scripts/homeassistant-run-on-boot.service /etc/systemd/system
 sudo systemctl daemon-reload
 
-rm ~/flash-tv-scripts/video_capture
+rm -r ~/flash-tv-scripts/video_capture
 
 #from /harddisk/FLASH_TV_installation
 
