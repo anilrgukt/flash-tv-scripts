6b0b70bd59edaf724dd2f7bf769481bd1555ec91
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-27 15:07:11 -0600

    async now

--- full_setup_auto.sh
+++ full_setup_auto.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-bash -x ~/flash-tv-scripts/rename-ids.sh &
-bash -x ~/flash-tv-scripts/FLASH_filesetup.sh &
-bash -x ~/flash-tv-scripts/install_scripts/full_install_auto.sh &
-wait
+bash -x ~/flash-tv-scripts/rename-ids.sh
+bash -x ~/flash-tv-scripts/FLASH_filesetup.sh
+bash -x ~/flash-tv-scripts/install_scripts/full_install_auto.sh
