c8dea8ee02acdfcff989f5e6fef99f8da0e0d8e9
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-27 15:16:03 -0600

    Update enable-services.sh

--- enable-services.sh
+++ enable-services.sh
@@ -4,4 +4,6 @@ sudo systemctl enable homeassistant-run-on-boot.service
 sudo systemctl start flash-periodic-restart.service
 sudo systemctl start flash-run-on-boot.service
 sudo systemctl start homeassistant-run-on-boot.service
-
+sudo systemctl status flash-periodic-restart.service
+sudo systemctl status flash-run-on-boot.service
+sudo systemctl status homeassistant-run-on-boot.service
