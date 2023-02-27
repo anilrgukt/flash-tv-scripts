e1a94b348b0b869d501135d0852e9535e0cf3e8d
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-27 12:05:40 -0600

    Update rename-ids.sh

--- rename-ids.sh
+++ rename-ids.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-echo 'Enter FLASH device ID (3 digits):'
-read devID
-echo 'Enter family ID (probably 3 digits):'
-read famID
+read -p 'Enter FLASH device ID (3 digits):' deviceID
+read -p 'Enter family ID (3 digits probably):' familyID
 
-sudo sed -i "s/XXX/$devID/g" '~/flash-tv-scripts/flash-periodic-restart.service'
-sudo sed -i "s/123/$famID/g" '~/flash-tv-scripts/flash-periodic-restart.service'
-sudo sed -i "s/XXX/$devID/g" '~/flash-tv-scripts/flash-periodic-restart.sh'
-sudo sed -i "s/123/$famID/g" '~/flash-tv-scripts/flash-periodic-restart.sh'
-sudo sed -i "s/XXX/$devID/g" '~/flash-tv-scripts/flash-run-on-boot.service'
-sudo sed -i "s/123/$famID/g" '~/flash-tv-scripts/flash-run-on-boot.service'
-sudo sed -i "s/XXX/$devID/g" '~/flash-tv-scripts/homeassistant-run-on-boot.service'
-sudo sed -i "s/123/$famID/g" '~/flash-tv-scripts/homeassistant-run-on-boot.service'
-sudo sed -i "s/XXX/$devID/g" '~/flash-tv-scripts/sh_scripts/data_details.sh'
-sudo sed -i "s/123/$famID/g" '~/flash-tv-scripts/sh_scripts/data_details.sh'
+sed -i "s/XXX/$deviceID/g" '~/flash-tv-scripts/flash-periodic-restart.service'
+sed -i "s/123/$familyID/g" '~/flash-tv-scripts/flash-periodic-restart.service'
+sed -i "s/XXX/$deviceID/g" '~/flash-tv-scripts/flash-periodic-restart.sh'
+sed -i "s/123/$familyID/g" '~/flash-tv-scripts/flash-periodic-restart.sh'
+sed -i "s/XXX/$deviceID/g" '~/flash-tv-scripts/flash-run-on-boot.service'
+sed -i "s/123/$familyID/g" '~/flash-tv-scripts/flash-run-on-boot.service'
+sed -i "s/XXX/$deviceID/g" '~/flash-tv-scripts/homeassistant-run-on-boot.service'
+sed -i "s/123/$familyID/g" '~/flash-tv-scripts/homeassistant-run-on-boot.service'
+sed -i "s/XXX/$deviceID/g" '~/flash-tv-scripts/sh_scripts/data_details.sh'
+sed -i "s/123/$familyID/g" '~/flash-tv-scripts/sh_scripts/data_details.sh'
