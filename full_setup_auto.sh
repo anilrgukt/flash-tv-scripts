4fe13dd2afd3a0fc0f823e0129042879a95d96d8
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-27 15:55:05 -0600

    Update full_setup_auto.sh

--- full_setup_auto.sh
+++ full_setup_auto.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bash ~/flash-tv-scripts/rename_ids.sh
+bash ~/flash-tv-scripts/setup_ids.sh
 bash ~/flash-tv-scripts/FLASH_filesetup.sh
 bash ~/flash-tv-scripts/install_scripts/full_install_auto.sh
