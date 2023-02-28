94d359505c03bc29dab7df722475d51475745650
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-28 11:50:56 -0600

    removed service setup

--- file_setup.sh
+++ file_setup.sh
@@ -5,15 +5,6 @@
 sudo sed -i 's/ATTR{hctosys}=="1"/ATTR{hctosys}=="0"/g' /lib/udev/rules.d/50-udev-default.rules 
 sudo hwclock -w
 
-# Copy, load, and enable services
-sudo cp ~/flash-tv-scripts/flash-run-on-boot.service /etc/systemd/system
-sudo cp ~/flash-tv-scripts/flash-periodic-restart.service /etc/systemd/system
-sudo cp ~/flash-tv-scripts/homeassistant-run-on-boot.service /etc/systemd/system
-sudo systemctl daemon-reload
-sudo systemctl enable flash-periodic-restart.service
-sudo systemctl enable flash-run-on-boot.service
-sudo systemctl enable homeassistant-run-on-boot.service
-
 # Remove video capture folder just in case as it's unnecessary for Jetsons
 rm -r ~/flash-tv-scripts/video_capture
 
