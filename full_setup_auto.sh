457b62ad95424009a2cc7144a83ae725c79c0fc4
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-28 10:08:22 -0600

    logging prompt

--- full_setup_auto.sh
+++ full_setup_auto.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-bash ~/flash-tv-scripts/setup_ids.sh
-bash ~/flash-tv-scripts/FLASH_filesetup.sh
-bash ~/flash-tv-scripts/install_scripts/full_install_auto.sh
+read -p 'Enable logging (y/n)?' answer
+if [ "$answer" = "${answer#[Yy]}" ] ;then
+    bash -x ~/flash-tv-scripts/FLASH_filesetup.sh
+    bash -x ~/flash-tv-scripts/install_scripts/full_install_auto.sh
+else
+    bash ~/flash-tv-scripts/FLASH_filesetup.sh
+    bash ~/flash-tv-scripts/install_scripts/full_install_auto.sh
+fi
+
+
+
+
