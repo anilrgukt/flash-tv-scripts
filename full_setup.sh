b92bf24fc1911316c26b803d036e83dcca04c758
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-28 10:31:36 -0600

    Update full_setup.sh

--- full_setup.sh
+++ full_setup.sh
@@ -2,10 +2,10 @@
 
 read -p 'Enable logging (y/n)?' answer
 if [ "$answer" = "${answer#[Yy]}" ] ;then
-    bash -x ~/flash-tv-scripts/full_file_setup.sh
+    bash -x ~/flash-tv-scripts/file_setup.sh
     bash -x ~/flash-tv-scripts/install_scripts/full_install.sh
 else
-    bash ~/flash-tv-scripts/full_file_setup.sh
+    bash ~/flash-tv-scripts/file_setup.sh
     bash ~/flash-tv-scripts/install_scripts/full_install.sh
 fi
 
