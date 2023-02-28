aa9f22dd1097b29b0aaa82161d1693e825bd381c
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-28 10:29:51 -0600

    added RTC setup

--- full_file_setup.sh
+++ full_file_setup.sh
@@ -3,9 +3,11 @@
 read -p 'Enter FLASH device ID (3 digits):' deviceID
 read -p 'Enter family ID (3 digits probably):' familyID
 
+# Create data directory
 mkdir "~/data"
 mkdir "$~/data/{familyID}${deviceID}_data"
 
+# Replace device and family ID placeholders with input
 sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash-periodic-restart.service
 sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash-periodic-restart.service
 sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash_periodic_restart.sh
@@ -19,6 +21,11 @@ sed -i "s/123/$familyID/g" ~/flash-tv-scripts/homeassistant-run-on-boot.service
 sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/sh_scripts/data_details.sh
 sed -i "s/123/$familyID/g" ~/flash-tv-scripts/sh_scripts/data_details.sh
 
+# Set up RTC
+sudo sed -i 's/ATTR{hctosys}=="1"/ATTR{hctosys}=="0"/g' /lib/udev/rules.d/50-udev-default.rules 
+sudo hwclock -w
+
+# Copy, load, and enable services
 sudo cp ~/flash-tv-scripts/flash-run-on-boot.service /etc/systemd/system
 sudo cp ~/flash-tv-scripts/flash-periodic-restart.service /etc/systemd/system
 sudo cp ~/flash-tv-scripts/homeassistant-run-on-boot.service /etc/systemd/system
@@ -27,6 +34,7 @@ sudo systemctl enable flash-periodic-restart.service
 sudo systemctl enable flash-run-on-boot.service
 sudo systemctl enable homeassistant-run-on-boot.service
 
+# Remove video capture folder just in case as it's unnecessary for Jetsons
 rm -r ~/flash-tv-scripts/video_capture
 
 #from /harddisk/FLASH_TV_installation
