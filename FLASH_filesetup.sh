d99241729582d0cf72fd628352ed9f6ce8037378
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-28 10:00:20 -0600

    combined with setup_ids.sh

--- FLASH_filesetup.sh
+++ FLASH_filesetup.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 # FILE setup
+read -p 'Enter FLASH device ID (3 digits):' deviceID
+read -p 'Enter family ID (3 digits probably):' familyID
+
+mkdir "~/data"
+mkdir "$~/data/{familyID}${deviceID}_data"
+
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash-periodic-restart.service
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash-periodic-restart.service
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash_periodic_restart.sh
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash_periodic_restart.sh
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash-run-on-boot.service
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash-run-on-boot.service
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash_run_on_boot.sh
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash_run_on_boot.sh
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/homeassistant-run-on-boot.service
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/homeassistant-run-on-boot.service
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/sh_scripts/data_details.sh
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/sh_scripts/data_details.sh
 
 sudo cp ~/flash-tv-scripts/flash-run-on-boot.service /etc/systemd/system
 sudo cp ~/flash-tv-scripts/flash-periodic-restart.service /etc/systemd/system
