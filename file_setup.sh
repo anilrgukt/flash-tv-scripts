06899435620c4ca1e31e0d84dd34dcea73b59628
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-28 11:42:39 -0600

    removed id setup

--- file_setup.sh
+++ file_setup.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 # FILE setup
-read -p 'Enter FLASH device ID (3 digits):' deviceID
-read -p 'Enter family ID (3 digits probably):' familyID
-
-# Create data directory
-mkdir "~/data"
-mkdir "$~/data/{familyID}${deviceID}_data"
-
-# Replace device and family ID placeholders with input
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash-periodic-restart.service
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash-periodic-restart.service
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash_periodic_restart.sh
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash_periodic_restart.sh
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash-run-on-boot.service
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash-run-on-boot.service
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash_run_on_boot.sh
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash_run_on_boot.sh
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/homeassistant-run-on-boot.service
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/homeassistant-run-on-boot.service
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/sh_scripts/data_details.sh
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/sh_scripts/data_details.sh
 
 # Set up RTC
 sudo sed -i 's/ATTR{hctosys}=="1"/ATTR{hctosys}=="0"/g' /lib/udev/rules.d/50-udev-default.rules 
