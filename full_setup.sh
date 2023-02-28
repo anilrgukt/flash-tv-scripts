cc3025aa52cc548f0498190d638ae130c95191ed
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-28 11:46:10 -0600

    added id_setup

--- full_setup.sh
+++ full_setup.sh
@@ -2,9 +2,11 @@
 
 read -p 'Enable logging (y/n)?' answer
 if [ "$answer" = "${answer#[Yy]}" ] ;then
+    bash -x ~/flash-tv-scripts/id_setup.sh
     bash -x ~/flash-tv-scripts/file_setup.sh
     bash -x ~/flash-tv-scripts/install_scripts/full_install.sh
 else
+    bash ~/flash-tv-scripts/id_setup.sh
     bash ~/flash-tv-scripts/file_setup.sh
     bash ~/flash-tv-scripts/install_scripts/full_install.sh
 fi
