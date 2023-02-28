8a8c91deecd14b59483a9c78b1212af2ee6b2e05
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-28 09:57:22 -0600

    Update and rename enable-services.sh to test-services.sh

--- test-services.sh
+++ test-services.sh
@@ -1,6 +1,3 @@
-sudo systemctl enable flash-periodic-restart.service
-sudo systemctl enable flash-run-on-boot.service
-sudo systemctl enable homeassistant-run-on-boot.service
 sudo systemctl start flash-periodic-restart.service
 sudo systemctl start flash-run-on-boot.service
 sudo systemctl start homeassistant-run-on-boot.service
