0e93086f701789e9105aec134ff621a6d05c2317
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-28 11:44:07 -0600

    Create id_setup.sh

---
+++ id/id_setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Take input
+read -p 'Enter FLASH device ID (3 digits):' deviceID
+read -p 'Enter family ID (3 digits probably):' familyID
+
+# Create data directory
+mkdir "~/data"
+mkdir "$~/data/{familyID}${deviceID}_data"
+
+# Replace device and family ID placeholders with input
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash-periodic-restart.service
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash-periodic-restart.service
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash_periodic_restart.sh
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash_periodic_restart.sh
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash-run-on-boot.service
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash-run-on-boot.service
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash_run_on_boot.sh
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash_run_on_boot.sh
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/homeassistant-run-on-boot.service
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/homeassistant-run-on-boot.service
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/sh_scripts/data_details.sh
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/sh_scripts/data_details.sh
