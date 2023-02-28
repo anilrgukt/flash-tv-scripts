626b96a1babcd32eb62415e93c220c71a69bdf85
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-02-28 10:21:34 -0600

    Update and rename full_setup_auto.sh to full_setup.sh

--- full_setup.sh
+++ full_setup.sh
@@ -2,10 +2,10 @@
 
 read -p 'Enable logging (y/n)?' answer
 if [ "$answer" = "${answer#[Yy]}" ] ;then
-    bash -x ~/flash-tv-scripts/FLASH_filesetup.sh
+    bash -x ~/flash-tv-scripts/full_file_setup.sh
     bash -x ~/flash-tv-scripts/install_scripts/full_install_auto.sh
 else
-    bash ~/flash-tv-scripts/FLASH_filesetup.sh
+    bash ~/flash-tv-scripts/full_file_setup.sh
     bash ~/flash-tv-scripts/install_scripts/full_install_auto.sh
 fi
 
