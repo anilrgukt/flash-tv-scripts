9a521637096d4bc4c59ab6b4e0f7a48ea31a953d
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-01 12:30:11 -0600

    Update id_setup.sh

--- setup_scripts/id_setup.sh
+++ setup_scripts/id_setup.sh
@@ -12,18 +12,18 @@ mkdir ~/data
 mkdir ~/data/${familyID}${deviceID}_data
 
 # Replace device and family ID placeholders with input
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash-periodic-restart.service
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash-periodic-restart.service
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash_periodic_restart.sh
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash_periodic_restart.sh
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash-run-on-boot.service
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash-run-on-boot.service
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/flash_run_on_boot.sh
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/flash_run_on_boot.sh
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/homeassistant-run-on-boot.service
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/homeassistant-run-on-boot.service
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/sh_scripts/data_details.sh
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/sh_scripts/data_details.sh
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/services/flash-periodic-restart.service
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/services/flash-periodic-restart.service
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/services/flash_periodic_restart.sh
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/services/flash_periodic_restart.sh
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/services/flash-run-on-boot.service
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/services/flash-run-on-boot.service
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/services/flash_run_on_boot.sh
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/services/flash_run_on_boot.sh
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/services/homeassistant-run-on-boot.service
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/services/homeassistant-run-on-boot.service
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/runtime_scripts/data_details.sh
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/runtime_scripts/data_details.sh
 sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/install_scripts/configuration.yaml
 sed -i "s/123/$familyID/g" ~/flash-tv-scripts/install_scripts/configuration.yaml
 
