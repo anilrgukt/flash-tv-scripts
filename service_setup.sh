f422ab0017c0b39da176a3c9474638e76b8b8f2e
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-01 11:52:50 -0600

    added disabling of services first

--- service_setup.sh
+++ service_setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Disable currently running services
+sudo systemctl disable flash-periodic-restart.service
+sudo systemctl disable flash-run-on-boot.service
+sudo systemctl disable homeassistant-run-on-boot.service
+
 # Copy, load, and enable services
 sudo cp ~/flash-tv-scripts/flash-run-on-boot.service /etc/systemd/system
 sudo cp ~/flash-tv-scripts/flash-periodic-restart.service /etc/systemd/system
