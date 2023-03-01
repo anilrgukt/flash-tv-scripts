585150952c60133d00067e8d43c9337428fd63d3
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-01 14:10:23 -0600

    Update stop_services.sh

--- stop_services.sh
+++ stop_services.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Disable all services
 sudo systemctl disable flash-periodic-restart.service
 sudo systemctl disable flash-run-on-boot.service
 sudo systemctl disable homeassistant-run-on-boot.service
+
+# Stop all services
 sudo systemctl stop flash-periodic-restart.service
 sudo systemctl stop flash-run-on-boot.service
 sudo systemctl stop homeassistant-run-on-boot.service
