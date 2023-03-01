171a95c7835b51505b2b5acee417567f73d5de38
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-01 14:10:52 -0600

    Update test_services.sh

--- test_services.sh
+++ test_services.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Start all services
 sudo systemctl start flash-periodic-restart.service
 sudo systemctl start flash-run-on-boot.service
 sudo systemctl start homeassistant-run-on-boot.service
+
+#Display the status of all services
 sudo systemctl status flash-periodic-restart.service
 sudo systemctl status flash-run-on-boot.service
 sudo systemctl status homeassistant-run-on-boot.service
