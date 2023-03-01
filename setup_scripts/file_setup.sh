dd975d049c2ddbdd0b26d501a0b2d74437edd7d2
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-01 13:25:09 -0600

    added copying of new Python scripts

--- setup_scripts/file_setup.sh
+++ setup_scripts/file_setup.sh
@@ -5,7 +5,7 @@
 sudo sed -i 's/ATTR{hctosys}=="1"/ATTR{hctosys}=="0"/g' /lib/udev/rules.d/50-udev-default.rules 
 sudo hwclock -w
 
-#from /harddisk/FLASH_TV_installation
+# from /harddisk/FLASH_TV_installation
 
 #FLASH_TV ----> in /home/$USER
 cp -r /media/$USER/696f0b73-ad9f-44a2-9cbd-fd09be1e4164/FLASH_TV_installation/FLASH_TV /home/$USER
@@ -24,3 +24,7 @@ sudo cp -r /media/$USER/696f0b73-ad9f-44a2-9cbd-fd09be1e4164/FLASH_TV_installati
 cp -r /media/$USER/696f0b73-ad9f-44a2-9cbd-fd09be1e4164/FLASH_TV_installation/FLASH_TV_v2 ~/Desktop
 #FLASH_TV_v3 ----> in /home/$USER/Desktop
 cp -r /media/$USER/696f0b73-ad9f-44a2-9cbd-fd09be1e4164/FLASH_TV_installation/FLASH_TV_v3 ~/Desktop
+
+# New Python scripts
+
+cp -r ~/flash-tv-scripts/python_scripts ~/Desktop/FLASH_TV_v3
