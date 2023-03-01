034b98cdde00f579215f000140198778fc9b2ab4
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-01 12:28:41 -0600

    Update family_change.sh

--- family_change.sh
+++ family_change.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # MUST DELETE AND RECLONE flash-tv-scripts directory BEFORE RUNNING THIS OR IT WILL NOT WORK PROPERLY
 
-bash -x ~/flash-tv-scripts/id_setup.sh
-bash -x ~/flash-tv-scripts/service_setup.sh
+bash -x ~/flash-tv-scripts/setup_scripts/id_setup.sh
+bash -x ~/flash-tv-scripts/setup_scripts/service_setup.sh
