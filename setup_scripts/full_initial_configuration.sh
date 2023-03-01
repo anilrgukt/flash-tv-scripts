f077f61cbf3d3e0bfde529ccae9cb1acf10cde30
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-01 12:24:22 -0600

    Update and rename full_initial_setup.sh to full_initial_configuration.sh

---
+++ setup_scripts/full_initial_configuration.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+bash -x ~/flash-tv-scripts/setup_scripts/id_setup.sh
+bash -x ~/flash-tv-scripts/setup_scripts/file_setup.sh
+bash -x ~/flash-tv-scripts/setup_scripts/service_setup.sh
+bash -x ~/flash-tv-scripts/install_scripts/full_install.sh
--- setup_scripts/full_initial_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-bash -x ~/flash-tv-scripts/id_setup.sh
-bash -x ~/flash-tv-scripts/file_setup.sh
-bash -x ~/flash-tv-scripts/service_setup.sh
-bash -x ~/flash-tv-scripts/install_scripts/full_install.sh
