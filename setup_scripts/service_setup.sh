40ee6eaacffbaa301fd0eba0bb30f552886ec72f
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-01 12:54:35 -0600

    Update service_setup.sh

--- setup_scripts/service_setup.sh
+++ setup_scripts/service_setup.sh
@@ -6,9 +6,9 @@ sudo systemctl disable flash-run-on-boot.service
 sudo systemctl disable homeassistant-run-on-boot.service
 
 # Copy, load, and enable services
-sudo cp ~/flash-tv-scripts/flash-run-on-boot.service /etc/systemd/system
-sudo cp ~/flash-tv-scripts/flash-periodic-restart.service /etc/systemd/system
-sudo cp ~/flash-tv-scripts/homeassistant-run-on-boot.service /etc/systemd/system
+sudo cp ~/flash-tv-scripts/services/flash-run-on-boot.service /etc/systemd/system
+sudo cp ~/flash-tv-scripts/services/flash-periodic-restart.service /etc/systemd/system
+sudo cp ~/flash-tv-scripts/services/homeassistant-run-on-boot.service /etc/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable flash-periodic-restart.service
 sudo systemctl enable flash-run-on-boot.service
