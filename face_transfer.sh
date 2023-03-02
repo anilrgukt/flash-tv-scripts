5862ec9f9b64a94fe11416e979d53776a488c9da
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-02 16:05:45 -0600

    Create face_transfer.sh

---
+++ face_transfer.sh
@@ -0,0 +1,11 @@
+faces = `ls ~/data/123XXX_data/123XXX_faces/`
+
+read -p 'Enter the ID of the device you are transferring faces FROM (3 digits):' olddeviceID
+read -p 'Enter the ID of the device you are transferring faces TO (3 digits probably):' newdeviceID
+
+for face in $faces
+do
+  mv -v "$face" "${face/olddeviceID/newdeviceID}"
+done
+
+
