c2272b57dc453264159f47fcafd7ebe04554ca94
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-02 10:09:13 -0600

    added enabling to test

--- test_services.sh
+++ test_services.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Enable all services
+sudo systemctl enable flash-periodic-restart.service
+sudo systemctl enable flash-run-on-boot.service
+sudo systemctl enable homeassistant-run-on-boot.service
+
 # Start all services
 sudo systemctl start flash-periodic-restart.service
 sudo systemctl start flash-run-on-boot.service
