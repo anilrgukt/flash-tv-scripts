2de48079d116641fbfd496b562113aa73250e2a2
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-02 16:43:17 -0600

    added folder renaming

--- runtime_scripts/face_id_transfer.sh
+++ runtime_scripts/face_id_transfer.sh
@@ -1,12 +1,21 @@
-faces=`ls ~/data/123XXX_data/123XXX_faces/`
-cd ~/data/123XXX_data/123XXX_faces/
+#!/bin/bash
 
 read -p 'Enter the ID of the device you are transferring faces FROM (3 digits):' olddeviceID
 read -p 'Enter the ID of the device you are transferring faces TO (3 digits):' newdeviceID
 
-for face in $faces
+facefolder=`ls ~/data/123XXX_data/ | grep faces`
+
+cd ~/data/123XXX_data/
+
+mv -v "$facefolder" "${facefolder/$olddeviceID/$newdeviceID}"
+
+faceimages=`ls ~/data/123XXX_data/123XXX_faces/`
+
+cd ~/data/123XXX_data/123XXX_faces/
+
+for faceimage in $faceimages
 do
-  mv -v "$face" "${face/$olddeviceID/$newdeviceID}"
+  mv -v "$faceimage" "${faceimage/$olddeviceID/$newdeviceID}"
 done
 
 
