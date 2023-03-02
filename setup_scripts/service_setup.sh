ff91b50031f50c62f0d849cba4140e3acb95728c
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-02 10:51:39 -0600

    changed path

--- setup_scripts/service_setup.sh
+++ setup_scripts/service_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Disable and stop currently running services
-bash -x ~/flash-tv-scripts/stop_services.sh
+bash -x ~/flash-tv-scripts/services/stop_services.sh
 
 # Copy, load, and enable services
 sudo cp ~/flash-tv-scripts/services/flash-run-on-boot.service /etc/systemd/system
