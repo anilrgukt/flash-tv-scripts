b36872c799025d4f6ca79b84e69a3c33d265f75f
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-16 11:44:15 -0500

    moved RTC clock setup here so that it runs with participant_change.sh

--- setup_scripts/service_setup.sh
+++ setup_scripts/service_setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set up RTC
+sudo sed -i 's/ATTR{hctosys}=="1"/ATTR{hctosys}=="0"/g' /lib/udev/rules.d/50-udev-default.rules 
+sudo hwclock -w
+
 # Disable and stop currently running services
 bash -x ~/flash-tv-scripts/services/stop_services.sh
 
