cc125c51c08fd22274837072688106e5e0feaf0f
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-22 15:21:44 -0500

    removed enabling of services as it causes too many issues with setup later

--- setup_scripts/service_setup.sh
+++ setup_scripts/service_setup.sh
@@ -12,6 +12,3 @@ sudo cp ~/flash-tv-scripts/services/flash-run-on-boot.service /etc/systemd/syste
 sudo cp ~/flash-tv-scripts/services/flash-periodic-restart.service /etc/systemd/system
 sudo cp ~/flash-tv-scripts/services/homeassistant-run-on-boot.service /etc/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable flash-periodic-restart.service
-sudo systemctl enable flash-run-on-boot.service
-sudo systemctl enable homeassistant-run-on-boot.service
