75498b262727bdd03de5defbc37e49b21baa33f6
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-22 14:12:46 -0500

    fixed wording

--- participant_change.sh
+++ participant_change.sh
@@ -2,11 +2,11 @@
 # MUST DELETE AND RECLONE the flash-tv-scripts folder BEFORE RUNNING THIS OR IT WILL NOT WORK PROPERLY
 
 # Setting up plug ID for Home Assistant config file
-read -p 'Enter plug ID (4 digits) or YYYY if the plug is not yet ready:' plugID
+read -p 'Enter plug ID (4 characters lowercase) or YYYY (uppercase) if the plug is not yet ready:' plugID
 
 sed -i "s/YYYY/$plugID/g" ~/flash-tv-scripts/install_scripts/configuration.yaml
 
-# Copying
+# Copying the config file to the Home Assistant directory
 cp ~/flash-tv-scripts/install_scripts/configuration.yaml ~/.homeassistant
 
 bash -x ~/flash-tv-scripts/setup_scripts/id_setup.sh
