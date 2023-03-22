e3b8a4803b3b36054322e94aa7c4ed9a0f540f56
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-22 14:13:19 -0500

    revert

--- full_initial_configuration.sh
+++ full_initial_configuration.sh
@@ -4,7 +4,3 @@ bash -x ~/flash-tv-scripts/setup_scripts/id_setup.sh
 bash -x ~/flash-tv-scripts/setup_scripts/file_setup.sh
 bash -x ~/flash-tv-scripts/setup_scripts/service_setup.sh
 bash -x ~/flash-tv-scripts/install_scripts/full_install.sh
-
-# Set up RTC
-sudo sed -i 's/ATTR{hctosys}=="1"/ATTR{hctosys}=="0"/g' /lib/udev/rules.d/50-udev-default.rules 
-sudo hwclock -w
