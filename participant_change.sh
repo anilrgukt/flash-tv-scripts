7e3e64edab42d3eba1db5e39367b1ce1e4900e8a
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-03-31 16:18:23 -0500

    added plug ID verification

--- participant_change.sh
+++ participant_change.sh
@@ -4,12 +4,7 @@
 # Setting up plug ID for Home Assistant config file
 read -p 'Enter plug ID (4 characters lowercase) or YYYY (uppercase) if the plug is not yet ready:' plugID
 
-sed -i "s/YYYY/$plugID/g" ~/flash-tv-scripts/install_scripts/configuration.yaml
-
-bash -x ~/flash-tv-scripts/setup_scripts/id_setup.sh
-bash -x ~/flash-tv-scripts/setup_scripts/service_setup.sh
-
-zenity --question --title="Verify Plug ID" --width 500 --height 100 --text="Please verify the following details\nPlug ID: $plugID\nFamily ID: $familyID\nDevice ID: $deviceID" --no-wrap
+zenity --question --title="Verify Plug ID" --width 500 --height 100 --text="Please verify the following details\nPlug ID: $plugID" --no-wrap
 user_resp=$?
 
 if [ $user_resp -eq 1 ]; then
@@ -17,6 +12,12 @@ if [ $user_resp -eq 1 ]; then
 	zenity --warning --text="Exiting the code since data details are not correct. Please modify them and restart the script."
 	exit 
 fi
+
+sed -i "s/YYYY/$plugID/g" ~/flash-tv-scripts/install_scripts/configuration.yaml
+
+bash -x ~/flash-tv-scripts/setup_scripts/id_setup.sh
+bash -x ~/flash-tv-scripts/setup_scripts/service_setup.sh
+
 
 # Copying the config file to the Home Assistant directory
 cp ~/flash-tv-scripts/install_scripts/configuration.yaml ~/.homeassistant
