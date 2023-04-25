d7ea54d8b7b930a7c4eb44aec61f25785a7b1c75
anilrgukt <anil.rgukt@gmail.com>
2023-04-25 15:17:07 -0500

    added logging timedatectl status to the log files

--- services/flash_periodic_restart.sh
+++ services/flash_periodic_restart.sh
@@ -16,6 +16,7 @@ do
 	systemctl status flash-run-on-boot.service > "${logFile}/log_${dt}.txt"
 	systemctl stop flash-run-on-boot.service
 	systemctl status flash-run-on-boot.service > "${logFile}/logend_${dt}.txt"
+	timedatectl status > "${logFile}/timedate_${dt}.txt"
 	
 	pkill -9 -f test_vid_frames_batch_v7_2fps_frminp_newfv_rotate.py
 	
