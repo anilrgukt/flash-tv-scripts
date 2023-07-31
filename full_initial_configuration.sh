12490ba4c0f4a60c96d52641c17025d6c9fe26b7
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-07-31 15:13:44 -0500

    Added exit on non-zero error code from subscripts

--- full_initial_configuration.sh
+++ full_initial_configuration.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-#test
+# Set to exit on non-zero error code
+set -e
+
+# Run all setup files
 bash -x ~/flash-tv-scripts/setup_scripts/id_setup.sh
 bash -x ~/flash-tv-scripts/setup_scripts/file_setup.sh
 bash -x ~/flash-tv-scripts/setup_scripts/service_setup.sh
