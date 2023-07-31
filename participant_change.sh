1eb10205e68c54ed57e43f7569d740987ee7a620
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-07-31 15:06:35 -0500

    Added exit on non-zero error code

--- participant_change.sh
+++ participant_change.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # MUST DELETE AND RECLONE the flash-tv-scripts folder BEFORE RUNNING THIS OR IT WILL NOT WORK PROPERLY
 
+# Set to exit on non-zero error code
+set -e
+
 # Setting up plug ID for Home Assistant config file
 read -p 'Enter plug ID (4 characters lowercase) or YYYY (uppercase) if the plug is not yet ready:' plugID
 
@@ -10,7 +13,7 @@ user_resp=$?
 if [ $user_resp -eq 1 ]; then
 	#echo "Exitted the code $user_resp"
 	zenity --warning --text="Exiting the code since data details are not correct. Please modify them and restart the script."
-	exit 
+	exit 1
 fi
 
 sed -i "s/YYYY/$plugID/g" ~/flash-tv-scripts/install_scripts/configuration.yaml
