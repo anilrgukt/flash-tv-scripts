5e9d67688d29d9ed22d49256543f5a8072b59930
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-07-31 14:01:18 -0500

    added tegrastats 1s interval output to log file

--- services/flash_run_on_boot.sh
+++ services/flash_run_on_boot.sh
@@ -14,6 +14,8 @@ hwclock -s
 
 source /home/$usrName/py38/bin/activate
 
+tegrastats --interval 1000 --logfile /home/flashsysXXX/data/123XXX_data/123XXX_tegrastats.log &
+
 # runs a while loop for the flash-tv algorithm 
 while true;
 do
