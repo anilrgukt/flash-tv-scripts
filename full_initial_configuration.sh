88ea1d3caeb53cc88c4b21007cbef679656b3fcd
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-07-31 14:10:09 -0500

    added RTC setup at end again just in case

--- full_initial_configuration.sh
+++ full_initial_configuration.sh
@@ -5,3 +5,7 @@ bash -x ~/flash-tv-scripts/setup_scripts/id_setup.sh
 bash -x ~/flash-tv-scripts/setup_scripts/file_setup.sh
 bash -x ~/flash-tv-scripts/setup_scripts/service_setup.sh
 bash -x ~/flash-tv-scripts/install_scripts/full_install.sh
+
+# Set up RTC again just in case
+sudo sed -i 's/ATTR{hctosys}=="1"/ATTR{hctosys}=="0"/g' /lib/udev/rules.d/50-udev-default.rules 
+sudo hwclock -w
