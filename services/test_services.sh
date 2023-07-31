334d3790c53686c1ee90a0033a6f7f159d68da15
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-07-31 16:39:42 -0500

    Added testing of system-daily-reboot-test.service

--- services/test_services.sh
+++ services/test_services.sh
@@ -4,11 +4,13 @@
 sudo systemctl enable flash-periodic-restart.service
 sudo systemctl enable flash-run-on-boot.service
 sudo systemctl enable homeassistant-run-on-boot.service
+sudo systemctl enable system-daily-reboot-test.service
 
 # Start all services
 sudo systemctl start flash-periodic-restart.service
 sudo systemctl start flash-run-on-boot.service
 sudo systemctl start homeassistant-run-on-boot.service
+sudo systemctl start system-daily-reboot-test.service
 
 #Display the status of all services
-systemctl status flash-periodic-restart.service flash-run-on-boot.service homeassistant-run-on-boot.service
+systemctl status flash-periodic-restart.service flash-run-on-boot.service homeassistant-run-on-boot.service system-daily-reboot-test.service
