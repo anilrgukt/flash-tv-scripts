29a4fb6c1c12903fdc156489060ec85e8db9d0f9
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-07-31 15:35:12 -0500

    updated to using Python 3.11 for HA and hardset HA 6/3/2023 version

--- install_scripts/homeassistant_install.sh
+++ install_scripts/homeassistant_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #see homeassistant_setup.txt for how to initialize homeassistant after this
 
-#for Python 3.10 easy install
+#for Python 3.11 easy install
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
 
 #install homeassistant dependencies
-sudo apt-get install -y python3.10 python3.10-dev python3.10-venv python3-pip bluez libffi-dev libssl-dev libjpeg-dev zlib1g-dev autoconf build-essential libopenjp2-7 libtiff5 libturbojpeg0-dev tzdata
+sudo apt-get install -y python3.11 python3.11-dev python3.11-venv python3-pip bluez libffi-dev libssl-dev libjpeg-dev zlib1g-dev autoconf build-essential libopenjp2-7 libtiff5 libturbojpeg0-dev tzdata ffmpeg liblapack3 liblapack-dev libatlas-base-dev
 
 #set up homeassistant folder
 cd /home/$USER
@@ -15,14 +15,14 @@ mkdir ha
 #go to homeassistant directory
 cd ha
 
-#create Python 3.10 virtual environment if it doesn't already exist and activate it 
+#create Python 3.11 virtual environment if it doesn't already exist and activate it 
 if ! ls | grep -q "bin"
 then 
-	python3.10 -m venv .
+	python3.11 -m venv .
 fi
 source bin/activate
 
 #install Python dependencies and then homeassistant
 pip install --upgrade pip
 pip3 install wheel
-pip3 install homeassistant
+pip3 install homeassistant==2023.6.3
