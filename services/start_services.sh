d81f97e385bca80590f4aadd53466c773619ab07
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-08-01 09:35:49 -0500

    Rename

--- services/start_services.sh
+++ services/start_services.sh
@@ -4,13 +4,13 @@
 sudo systemctl enable flash-periodic-restart.service
 sudo systemctl enable flash-run-on-boot.service
 sudo systemctl enable homeassistant-run-on-boot.service
-sudo systemctl enable system-daily-reboot-test.service
+sudo systemctl enable daily-reboot.service
 
 # Start all services
 sudo systemctl start flash-periodic-restart.service
 sudo systemctl start flash-run-on-boot.service
 sudo systemctl start homeassistant-run-on-boot.service
-sudo systemctl start system-daily-reboot-test.service
+sudo systemctl start daily-reboot.service
 
 #Display the status of all services
-systemctl status flash-periodic-restart.service flash-run-on-boot.service homeassistant-run-on-boot.service system-daily-reboot-test.service
+systemctl status flash-periodic-restart.service flash-run-on-boot.service homeassistant-run-on-boot.service daily-reboot.service
