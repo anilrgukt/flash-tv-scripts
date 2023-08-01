759911c7b0c5551f5b04cf96ed5aae86a568d38b
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-08-01 09:34:52 -0500

    Added daily-reboot.service

--- services/restart_services.sh
+++ services/restart_services.sh
@@ -4,3 +4,4 @@
 sudo systemctl restart flash-periodic-restart.service
 sudo systemctl restart flash-run-on-boot.service
 sudo systemctl restart homeassistant-run-on-boot.service
+sudo systemctl restart daily-reboot.service
