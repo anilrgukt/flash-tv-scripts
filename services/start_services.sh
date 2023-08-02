889a15ea3564a74f55031a7d04b03428861ff730
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-08-02 16:23:24 -0500

    added sleep 1

--- services/start_services.sh
+++ services/start_services.sh
@@ -2,13 +2,19 @@
 
 # Enable all services
 sudo systemctl enable flash-periodic-restart.service
+sleep 1;
 sudo systemctl enable flash-run-on-boot.service
+sleep 1;
 sudo systemctl enable homeassistant-run-on-boot.service
+sleep 1;
 
 # Start all services
 sudo systemctl start flash-periodic-restart.service
+sleep 1;
 sudo systemctl start flash-run-on-boot.service
+sleep 1;
 sudo systemctl start homeassistant-run-on-boot.service
+sleep 1;
 
 #Display the status of all services
 systemctl status flash-periodic-restart.service flash-run-on-boot.service homeassistant-run-on-boot.service
