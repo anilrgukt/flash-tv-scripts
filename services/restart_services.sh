dfa5ca854fca625629ddee23dcf1c2ed27bf31ba
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-08-02 16:22:47 -0500

    added sleep 1 between restarts

--- services/restart_services.sh
+++ services/restart_services.sh
@@ -2,5 +2,7 @@
 
 # Restart all services
 sudo systemctl restart flash-periodic-restart.service
+sleep 1;
 sudo systemctl restart flash-run-on-boot.service
+sleep 1;
 sudo systemctl restart homeassistant-run-on-boot.service
