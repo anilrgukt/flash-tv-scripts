f5327ff7efa6d9a3798ef482e0ca2bd8d253f474
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-08-02 15:32:35 -0500

    Revert (deleted daily reboot)

--- services/restart_services.sh
+++ services/restart_services.sh
@@ -4,4 +4,3 @@
 sudo systemctl restart flash-periodic-restart.service
 sudo systemctl restart flash-run-on-boot.service
 sudo systemctl restart homeassistant-run-on-boot.service
-sudo systemctl restart daily-reboot.service
