bcdec1cdbc3d62388f5fda02ee37e319dcd21949
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-08-02 16:23:50 -0500

    added sleep 1

--- services/stop_services.sh
+++ services/stop_services.sh
@@ -2,12 +2,17 @@
 
 # Stop all services
 sudo systemctl stop flash-periodic-restart.service
+sleep 1;
 sudo systemctl stop flash-run-on-boot.service
+sleep 1;
 sudo systemctl stop homeassistant-run-on-boot.service
+sleep 1;
 
 # Disable all services
 sudo systemctl disable flash-periodic-restart.service
+sleep 1;
 sudo systemctl disable flash-run-on-boot.service
+sleep 1;
 sudo systemctl disable homeassistant-run-on-boot.service
 
 
