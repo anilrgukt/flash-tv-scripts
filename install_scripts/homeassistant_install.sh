daaab25da3f329b5201d8bfff0e6f6330c57bc7d
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-08-09 11:42:50 -0500

    change Python venv consistent version to 3.10

--- install_scripts/homeassistant_install.sh
+++ install_scripts/homeassistant_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #see homeassistant_setup.txt for how to initialize homeassistant after this
 
-#for Python 3.11 easy install
+#for Python 3.10 easy install
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
 
 #install homeassistant dependencies
-sudo apt-get install -y python3.11 python3.11-dev python3.11-venv python3-pip bluez libffi-dev libssl-dev libjpeg-dev zlib1g-dev autoconf build-essential libopenjp2-7 libtiff5 libturbojpeg0-dev tzdata ffmpeg liblapack3 liblapack-dev libatlas-base-dev
+sudo apt-get install -y python3.10 python3.10-dev python3.10-venv python3-pip bluez libffi-dev libssl-dev libjpeg-dev zlib1g-dev autoconf build-essential libopenjp2-7 libtiff5 libturbojpeg0-dev tzdata ffmpeg liblapack3 liblapack-dev libatlas-base-dev
 
 #set up homeassistant folder
 cd /home/$USER
@@ -15,10 +15,10 @@ mkdir ha
 #go to homeassistant directory
 cd ha
 
-#create Python 3.11 virtual environment if it doesn't already exist and activate it 
+#create Python 3.10 virtual environment if it doesn't already exist and activate it 
 if ! ls | grep -q "bin"
 then 
-	python3.11 -m venv .
+	python3.10 -m venv .
 fi
 source bin/activate
 
