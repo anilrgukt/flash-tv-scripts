efb75432e45d1cf5eef8ad13f988d9cccd2a813d
anilrgukt <anil.rgukt@gmail.com>
2023-08-17 14:20:36 -0500

    modified periodic restart to keep an eye on camera warnings in stderr.log

--- services/flash_check_camera_warnings.sh
+++ services/flash_check_camera_warnings.sh
@@ -8,6 +8,7 @@ do
     sleep 120;
     errlog=/home/$usrName/data/${famId}_data/${famId}_flash_logstderr.log
     if grep -w 'tryIoctl' $errlog; then
+        echo "camera warning exists!!" `date`
         systemctl stop flash-run-on-boot.service
         sleep 10
         grep -rl 'tryIoctl' $errlog | xargs sed -i 's/tryIoctl/tryIoctl_addressed/g'
