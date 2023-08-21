9a6e3c1a53981ed0de0991ab705bd10a8039eab6
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-08-21 17:42:42 -0500

    minor prompt fixes, added USB_backup_setup.sh

--- setup_scripts/id_setup.sh
+++ setup_scripts/id_setup.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-# Remove video capture folder just in case as it's unnecessary for Jetsons
+# Remove video capture and internal testing folder just in case as they are unnecessary for Jetsons
 rm -r ~/flash-tv-scripts/video_capture_scripts
+rm -r ~/flash-tv-scripts/internal_testing
 
 # Take input
 read -p 'Enter FLASH device ID (3 digits):' deviceID
 read -p 'Enter family ID (3 digits probably):' familyID
 
-zenity --question --title="Verify Device and Family ID" --width 500 --height 100 --text="Please verify the following details\nFamily ID: $familyID\nDevice ID: $deviceID" --no-wrap
+zenity --question --title="Verify Device and Family ID" --width 500 --height 100 --text="Please verify the following details:\n\nFamily ID: $familyID\n\nDevice ID: $deviceID"
+
 user_resp=$?
 
 if [ $user_resp -eq 1 ]; then
-	zenity --warning --text="Exiting the code since data details are not correct. Please modify them and restart the script."
+	zenity --warning --text="Exiting the code since the data details are not correct.\n\nPlease modify them and restart the script." -width 500 --height 100
 	exit 1
 fi
 
@@ -36,4 +38,6 @@ sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/runtime_scripts/face_id_transfer.s
 sed -i "s/123/$familyID/g" ~/flash-tv-scripts/runtime_scripts/face_id_transfer.sh
 sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/install_scripts/configuration.yaml
 sed -i "s/123/$familyID/g" ~/flash-tv-scripts/install_scripts/configuration.yaml
+sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/setup_scripts/USB_backup_setup.sh
+sed -i "s/123/$familyID/g" ~/flash-tv-scripts/setup_scripts/USB_backup_setup.sh
 
