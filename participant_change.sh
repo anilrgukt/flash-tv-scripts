b149a89932302bf15b81c1e2b2c1aa9a2e352b77
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-08-22 12:26:15 -0500

    changed dialog to zenity and changed order of setup

--- participant_change.sh
+++ participant_change.sh
@@ -2,7 +2,7 @@
 # MUST DELETE AND RECLONE the flash-tv-scripts folder BEFORE RUNNING THIS OR IT WILL NOT WORK PROPERLY
 
 # Setting up plug ID for Home Assistant config file
-read -p 'Enter plug ID (4 characters lowercase) or YYYY (uppercase) if the plug is not ready:' plugID
+plugID=$(zenity --entry --width 500 --height 100 --text="Enter plug ID (4 characters lowercase) or YYYY (uppercase) if the plug is not ready:")
 
 zenity --question --title="Verify Plug ID" --text="Please verify the following details:\n\nPlug ID: $plugID" --width 500 --height 100 
 
@@ -22,9 +22,9 @@ sed -i "s/YYYY/$plugID/g" ~/flash-tv-scripts/install_scripts/configuration.yaml
 
 bash -x ~/flash-tv-scripts/setup_scripts/id_setup.sh
 sleep 1;
-bash -x ~/flash-tv-scripts/setup_scripts/service_setup.sh
-sleep 1;
 bash -x ~/flash-tv-scripts/setup_scripts/USB_backup_setup.sh
+sleep 1;
+bash -x ~/flash-tv-scripts/setup_scripts/service_setup.sh
 sleep 1;
 
 # Copying the config file to the Home Assistant directory
