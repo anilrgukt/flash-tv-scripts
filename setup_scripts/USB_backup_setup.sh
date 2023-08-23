6c031d29edec8b99ae3aa7c470a76e312742297e
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-08-23 12:29:12 -0500

    rename

--- setup_scripts/USB_backup_setup.sh
+++ setup_scripts/USB_backup_setup.sh
@@ -21,17 +21,17 @@ then
 
 	export BORG_PASSPHRASE="$encoded_password"
 
-	echo "$encoded_password" > ~/flash-tv-scripts/setup_scripts/borg-passphrase-flashsys002.txt
+	echo "$encoded_password" > ~/flash-tv-scripts/setup_scripts/borg-passphrase-flashsysXXX.txt
 
 	usb_path=`lsblk -o NAME,TRAN,MOUNTPOINT | grep -A 1 -w usb | grep -v usb | awk '{print $2}'`
 
-	export BORG_REPO=$usb_path/USB_Backup_Data_flashsys002
+	export BORG_REPO=$usb_path/USB_Backup_Data_flashsysXXX
 
 	borg init -v --encryption=repokey
 
-	borg key export --paper :: > $usb_path/borg-encrypted-key-backup-flashsys002.txt
-	borg key export --paper :: > ~/borg-encrypted-key-backup-flashsys002.txt
-	borg key export --paper :: > ~/flash-tv-scripts/setup_scripts/borg-encrypted-key-backup-flashsys002.txt
+	borg key export --paper :: > $usb_path/borg-encrypted-key-backup-flashsysXXX.txt
+	borg key export --paper :: > ~/borg-encrypted-key-backup-flashsysXXX.txt
+	borg key export --paper :: > ~/flash-tv-scripts/setup_scripts/borg-encrypted-key-backup-flashsysXXX.txt
 
 else
 	zenity --warning --width 500 --height 100 --text="Exiting the code since the backup USB was not connected.\nPlease connect the backup USB and try again."
