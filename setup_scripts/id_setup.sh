9cca3f9a6916bf3fb5387f80d90ef2070c30c4c9
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-08-23 17:18:14 -0500

    rename

--- setup_scripts/id_setup.sh
+++ setup_scripts/id_setup.sh
@@ -8,10 +8,10 @@ deviceID=$(zenity --entry --width 500 --height 100 --text="Enter FLASH device ID
 
 familyID=$(zenity --entry --width 500 --height 100 --text="Enter family ID (3 digits for Study 4, P1-1[3 digits] for TECH):")
 
-zenity --question --title="Verify Device and Family ID" --width 500 --height 100 --text="Please verify the following details:\n\nFamily ID: $familyID\n\nDevice ID: $deviceID"
+zenity --question --title="Verify Device and Family ID" --width 500 --height 100 --text="Please verify the following details:\n\nFamily ID: ${familyID}\n\nDevice ID: ${deviceID}"
 user_resp=$?
 
-if [ $user_resp -eq 1 ]; then
+if [ ${user_resp} -eq 1 ]; then
 	zenity --warning --text="Exiting the code since the data details are not correct.\n\nPlease modify them and restart the script." --width 500 --height 100
 	exit 1
 fi
@@ -21,22 +21,22 @@ mkdir ~/data
 mkdir ~/data/${familyID}${deviceID}_data
 
 # Replace device and family ID placeholders with input
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/services/flash-periodic-restart.service
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/services/flash-periodic-restart.service
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/services/flash_periodic_restart.sh
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/services/flash_periodic_restart.sh
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/services/flash-run-on-boot.service
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/services/flash-run-on-boot.service
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/services/flash_run_on_boot.sh
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/services/flash_run_on_boot.sh
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/services/homeassistant-run-on-boot.service
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/services/homeassistant-run-on-boot.service
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/runtime_scripts/data_details.sh
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/runtime_scripts/data_details.sh
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/runtime_scripts/face_id_transfer.sh
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/runtime_scripts/face_id_transfer.sh
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/install_scripts/configuration.yaml
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/install_scripts/configuration.yaml
-sed -i "s/XXX/$deviceID/g" ~/flash-tv-scripts/setup_scripts/USB_backup_setup.sh
-sed -i "s/123/$familyID/g" ~/flash-tv-scripts/setup_scripts/USB_backup_setup.sh
+sed -i "s/XXX/${deviceID}/g" ~/flash-tv-scripts/services/flash-periodic-restart.service
+sed -i "s/123/${familyID}/g" ~/flash-tv-scripts/services/flash-periodic-restart.service
+sed -i "s/XXX/${deviceID}/g" ~/flash-tv-scripts/services/flash_periodic_restart.sh
+sed -i "s/123/${familyID}/g" ~/flash-tv-scripts/services/flash_periodic_restart.sh
+sed -i "s/XXX/${deviceID}/g" ~/flash-tv-scripts/services/flash-run-on-boot.service
+sed -i "s/123/${familyID}/g" ~/flash-tv-scripts/services/flash-run-on-boot.service
+sed -i "s/XXX/${deviceID}/g" ~/flash-tv-scripts/services/flash_run_on_boot.sh
+sed -i "s/123/${familyID}/g" ~/flash-tv-scripts/services/flash_run_on_boot.sh
+sed -i "s/XXX/${deviceID}/g" ~/flash-tv-scripts/services/homeassistant-run-on-boot.service
+sed -i "s/123/${familyID}/g" ~/flash-tv-scripts/services/homeassistant-run-on-boot.service
+sed -i "s/XXX/${deviceID}/g" ~/flash-tv-scripts/runtime_scripts/data_details.sh
+sed -i "s/123/${familyID}/g" ~/flash-tv-scripts/runtime_scripts/data_details.sh
+sed -i "s/XXX/${deviceID}/g" ~/flash-tv-scripts/runtime_scripts/face_id_transfer.sh
+sed -i "s/123/${familyID}/g" ~/flash-tv-scripts/runtime_scripts/face_id_transfer.sh
+sed -i "s/XXX/${deviceID}/g" ~/flash-tv-scripts/install_scripts/configuration.yaml
+sed -i "s/123/${familyID}/g" ~/flash-tv-scripts/install_scripts/configuration.yaml
+sed -i "s/XXX/${deviceID}/g" ~/flash-tv-scripts/setup_scripts/USB_backup_setup.sh
+sed -i "s/123/${familyID}/g" ~/flash-tv-scripts/setup_scripts/USB_backup_setup.sh
 
