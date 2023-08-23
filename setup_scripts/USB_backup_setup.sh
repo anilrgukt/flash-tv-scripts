30dd06a465f766de543543e62e720145c5abe166
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-08-23 17:11:44 -0500

    multiple changes

--- setup_scripts/USB_backup_setup.sh
+++ setup_scripts/USB_backup_setup.sh
@@ -1,43 +1,57 @@
 #!/bin/bash
 
-if lsusb | grep "SanDisk Corp. Ultra Fit" &>/dev/null
+if lsusb | grep -q "SanDisk Corp. Ultra Fit"
 then
+	
+	# Create temp file to store plaintext password without echoing it in terminal
 	temp_file=$(mktemp)
-	stty -echo
 	
 	zenity --entry --hide-text --width 500 --height 100 --text="Enter USB Backup Password:" > "$temp_file"
 	
-	encoded_password=`python3 ~/flash-tv-scripts/setup_scripts/check_and_encode_password.py "$temp_file"`
+	# Send password to be checked and encoded using cryptography modules in Python
+	encoded_password=`python3 /home/flashsys002/flash-tv-scripts/setup_scripts/check_and_encode_password.py "$temp_file"`
 	exit_code=$?
 	
+	# Overwrite and destroy temp file
 	shred -z -u "$temp_file"
 
-	stty echo
-	
 	if [ $exit_code -eq 1 ]; then
 		zenity --warning --width 500 --height 100 --text="Exiting the code since the password was incorrect.\nPlease restart the script and try again."
 		exit 1
 	fi
+	
+	FILE=/home/flashsys002/.bashrc
 
+	# Export and save encoded password as borg passphrase
 	export BORG_PASSPHRASE="$encoded_password"
+	
+	echo "$encoded_password" > /home/flashsys002/flash-tv-scripts/setup_scripts/borg-passphrase-flashsys002.txt
+	
+	BORG_PASSPHRASE_EXPORT="export BORG_PASSPHRASE="$encoded_password""
 
-	echo "$encoded_password" > ~/flash-tv-scripts/setup_scripts/borg-passphrase-flashsysXXX.txt
+	grep -q '.*BORG_PASSPHRASE.*' "$FILE" || echo "$BORG_PASSPHRASE_EXPORT" >> "$FILE"
+	sed -i "s@.*BORG_PASSPHRASE.*@$BORG_PASSPHRASE_EXPORT@" "$FILE"
 
+	# Get backup USB path
 	backup_usb_path=`lsblk -o NAME,TRAN,MOUNTPOINT | grep -A 1 -w usb | grep -v usb | awk '{print $2}'`
 
-	export BORG_REPO=$backup_usb_path/USB_Backup_Data_flashsysXXX
+	# Export and save borg repo path
+	export BORG_REPO=$backup_usb_path/USB_Backup_Data_flashsys002
 	
-	BORG_REPO_ENV_VAR="export BORG_REPO=\$backup_usb_path/USB_Backup_Data_flashsysXXX"
-	FILE="~/.bashrc"
-	grep -xqF -- "USB_Backup_Data_flashsys" "$FILE" || echo "$BORG_REPO_ENV_VAR" >> "$FILE"
+	BORG_REPO_EXPORT="export BORG_REPO=$backup_usb_path/USB_Backup_Data_flashsys002"
+	
+	grep -q '.*BORG_REPO.*' "$FILE" || echo "$BORG_REPO_EXPORT" >> "$FILE"
+	sed -i "s@.*BORG_REPO.*@$BORG_REPO_EXPORT@" "$FILE"
 
+	# Initialize borg repo
 	borg init -v --encryption=repokey
 
-	borg key export --paper :: > $backup_usb_path/borg-encrypted-key-backup-flashsysXXX.txt
-	borg key export --paper :: > ~/borg-encrypted-key-backup-flashsysXXX.txt
-	borg key export --paper :: > ~/flash-tv-scripts/setup_scripts/borg-encrypted-key-backup-flashsysXXX.txt
+	# Export borg encryption keys to multiple places for backup
+	borg key export --paper :: > $backup_usb_path/borg-encrypted-key-backup-flashsys002.txt
+	borg key export --paper :: > /home/flashsys002/borg-encrypted-key-backup-flashsys002.txt
+	borg key export --paper :: > /home/flashsys002/flash-tv-scripts/setup_scripts/borg-encrypted-key-backup-flashsys002.txt
 
 else
-	zenity --warning --width 500 --height 100 --text="Exiting the code since the backup USB was not connected.\nPlease connect the backup USB and try again."
+	zenity --warning --width 500 --height 100 --text="Exiting the code since the backup USB is not connected.\nPlease connect the backup USB and try again."
 	exit 1
 fi
