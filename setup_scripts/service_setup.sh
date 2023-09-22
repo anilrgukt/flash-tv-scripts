b86aeeb44c6920a2c9f11a9be63e7750f42604ab
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-09-22 16:10:18 -0500

    added py38 sourcing

--- setup_scripts/service_setup.sh
+++ setup_scripts/service_setup.sh
@@ -5,6 +5,7 @@ sudo sed -i 's/ATTR{hctosys}=="1"/ATTR{hctosys}=="0"/g' /lib/udev/rules.d/50-ude
 sudo systemctl restart systemd-timesyncd.service
 sleep 5;
 sudo hwclock -w
+source ~/py38/bin/activate
 python3 ~/flash-tv-scripts/python_scripts/update_ext_ds3231_from_dt_now.py
 
 # Disable and stop currently running services
