f661fa617dd11f0999507d29b90b7e7437bf74cd
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-09-22 14:57:44 -0500

    reformat

--- python_scripts/update_timedatectl_from_ext_ds3231.py
+++ python_scripts/update_timedatectl_from_ext_ds3231.py
@@ -35,9 +35,10 @@ int_rtc_data = lambda: [int(x) for x in rtc_data()]
 
 dec_rtc_data = lambda: [int(x.replace("0x", "")) for x in hex_rtc_data()]
 #print(f"RTC Dec: {dec_rtc_data()}")
-dt_for_hwclock = f"{dec_rtc_data()[5]:02}/{dec_rtc_data()[4]:02}/20{dec_rtc_data()[6]:02} {dec_rtc_data()[2]:02}:{dec_rtc_data()[1]:02}:{dec_rtc_data()[0]:02}"
+#dt_for_hwclock = f"{dec_rtc_data()[5]:02}/{dec_rtc_data()[4]:02}/20{dec_rtc_data()[6]:02} {dec_rtc_data()[2]:02}:{dec_rtc_data()[1]:02}:{dec_rtc_data()[0]:02}"
 dt_for_timedatectl = f"20{dec_rtc_data()[6]:02}-{dec_rtc_data()[5]:02}-{dec_rtc_data()[4]:02} {dec_rtc_data()[2]:02}:{dec_rtc_data()[1]:02}:{dec_rtc_data()[0]:02}"
-print(dt_for_hwclock)
+#print(dt_for_hwclock)
+print(dt_for_timedatectl)
 #command = ["sudo", "hwclock", "--set", "--date", dt_for_hwclock]
 command = ["sudo", "timedatectl", "set-time", dt_for_timedatectl]
 subprocess.run(command, check=True)
