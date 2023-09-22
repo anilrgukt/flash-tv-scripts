5446941009c452851e1e9e943e5a00fdd08007a7
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-09-22 15:24:45 -0500

    removed regular updating from hwclock just for testing in this branch

--- services/flash_run_on_boot.sh
+++ services/flash_run_on_boot.sh
@@ -9,11 +9,14 @@ export MXNET_HOME=/home/$usrName/mxnet
 export PYTHONPATH=$MXNET_HOME/python:$PYTHONPATH
 
 # run time-synchronization commands 
-if [[ $(sudo hwclock -s 1>/dev/null) ]]; then
-	echo "Error occurred when trying to update system time from internal RTC, switching to update from external RTC"
-	timedatectl set-ntp 0
-	python3 /home/$usrName/flash-tv-scripts/python-scripts/update_timedatectl_from_ext_ds3231.py
-fi
+# if [[ $(sudo hwclock -s 1>/dev/null) ]]; then
+# 	echo "Error occurred when trying to update system time from internal RTC, switching to update from external RTC"
+# 	timedatectl set-ntp 0
+# 	python3 /home/$usrName/flash-tv-scripts/python-scripts/update_timedatectl_from_ext_ds3231.py
+# fi
+
+timedatectl set-ntp 0
+python3 /home/$usrName/flash-tv-scripts/python-scripts/update_timedatectl_from_ext_ds3231.py
 
 source /home/$usrName/py38/bin/activate
 
