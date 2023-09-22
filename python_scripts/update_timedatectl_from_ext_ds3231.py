4ef0ea40b6e211fbb042fee06046984add167194
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-09-22 15:14:15 -0500

    changed var to lambda func to fix timing

--- python_scripts/update_timedatectl_from_ext_ds3231.py
+++ python_scripts/update_timedatectl_from_ext_ds3231.py
@@ -13,15 +13,15 @@ hex_rtc_data = lambda: [hex(x) for x in rtc_data()]
 dec_rtc_data = lambda: [int(x.replace("0x", "")) for x in hex_rtc_data()]
 #print(f"RTC Dec: {dec_rtc_data()}")
 
-#dt_for_hwclock = f"{dec_rtc_data()[5]:02}/{dec_rtc_data()[4]:02}/20{dec_rtc_data()[6]:02} {dec_rtc_data()[2]:02}:{dec_rtc_data()[1]:02}:{dec_rtc_data()[0]:02}"
-dt_for_timedatectl = f"20{dec_rtc_data()[6]:02}-{dec_rtc_data()[5]:02}-{dec_rtc_data()[4]:02} {dec_rtc_data()[2]:02}:{dec_rtc_data()[1]:02}:{dec_rtc_data()[0]:02}"
+#dt_for_hwclock = lambda: f"{dec_rtc_data()[5]:02}/{dec_rtc_data()[4]:02}/20{dec_rtc_data()[6]:02} {dec_rtc_data()[2]:02}:{dec_rtc_data()[1]:02}:{dec_rtc_data()[0]:02}"
+dt_for_timedatectl = lambda: f"20{dec_rtc_data()[6]:02}-{dec_rtc_data()[5]:02}-{dec_rtc_data()[4]:02} {dec_rtc_data()[2]:02}:{dec_rtc_data()[1]:02}:{dec_rtc_data()[0]:02}"
 
-#command = ["sudo", "hwclock", "--set", "--date", dt_for_hwclock]
+#command = ["sudo", "hwclock", "--set", "--date", dt_for_hwclock()]
 command1 = ["sudo", "timedatectl", "set-ntp", "0"]
-command2 = ["sudo", "timedatectl", "set-time", dt_for_timedatectl]
+command2 = ["sudo", "timedatectl", "set-time", dt_for_timedatectl()]
 subprocess.run(command1, check=True)
 subprocess.run(command2, check=True)
 bus.close()
 
-#print(print(f"Time for hwclock was be set to: {dt_for_hwclock}"))
-print(f"Time for timedatectl was set to: {dt_for_timedatectl}")
+#print(print(f"Time for hwclock was be set to: {dt_for_hwclock()}"))
+print(f"Time for timedatectl was set to: {dt_for_timedatectl()}")
