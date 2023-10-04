57c727a16df7c65b959250507ea11e31501367fa
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-10-04 16:21:53 -0500

    unnecessary

--- python_scripts/set_ext_ds3231_from_dt_now.py
+++ python_scripts/set_ext_ds3231_from_dt_now.py
@@ -29,7 +29,7 @@ def set_external_rtc_time():
         bus.write_i2c_block_data(RTC_ADDRESS, 0, get_current_time_bcd())
         bus.close()
         print(f"Time for external RTC was set to: {get_current_time_bcd()}")
-    except Exception:
+    except:
         print(traceback.format_exc())
         print("Failed to properly set time for external RTC. Please retry before continuing.")
         sys.exit(1)
