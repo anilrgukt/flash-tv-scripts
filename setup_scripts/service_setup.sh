c27d6ea0ab4ae8ed1bd1ba69b45097dfd495842e
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-10-04 16:44:46 -0500

    added error checking for setting time of all RTCs

--- setup_scripts/service_setup.sh
+++ setup_scripts/service_setup.sh
@@ -7,17 +7,26 @@ source ~/py38/bin/activate
 
 timedatectl set-ntp 1
 sleep 5;
+
 sudo systemctl restart systemd-timesyncd.service
 sleep 5;
-sudo hwclock -w
+
+if [[ $(sudo hwclock -w 1>/dev/null) ]]; then
+	zenity --warning --text="Exiting the code since the internal RTC time was unable to be set. Please check it and restart the script." --width 500 --height 100 
+	exit 1
+fi
 sleep 1;
-sudo hwclock --rtc /dev/rtc1 -w
+
+if [[ $(sudo hwclock --rtc /dev/rtc1 -w 1>/dev/null) ]]; then
+	echo "rtc1 was unable to be set"
+fi
 sleep 1;
+
 python3 ~/flash-tv-scripts/python_scripts/set_ext_ds3231_from_dt_now.py
 set_correctly=$?
 
 if [ $set_correctly -eq 1 ]; then
-	zenity --warning --text="Exiting the code since the external RTC time was unable to be set. Please check it and restart the script."
+	zenity --warning --text="Exiting the code since the external RTC time was unable to be set. Please check it and restart the script." --width 500 --height 100 
 	exit 1
 fi
 
