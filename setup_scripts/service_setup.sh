f3436e8174aa911c35102528699f7dcc5d38fe4e
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-10-04 16:46:13 -0500

    removed RTC setup from here

--- setup_scripts/service_setup.sh
+++ setup_scripts/service_setup.sh
@@ -1,35 +1,5 @@
 #!/bin/bash
 
-# Set up RTC
-sudo sed -i 's/ATTR{hctosys}=="1"/ATTR{hctosys}=="0"/g' /lib/udev/rules.d/50-udev-default.rules
-
-source ~/py38/bin/activate
-
-timedatectl set-ntp 1
-sleep 5;
-
-sudo systemctl restart systemd-timesyncd.service
-sleep 5;
-
-if [[ $(sudo hwclock -w 1>/dev/null) ]]; then
-	zenity --warning --text="Exiting the code since the internal RTC time was unable to be set. Please check it and restart the script." --width 500 --height 100 
-	exit 1
-fi
-sleep 1;
-
-if [[ $(sudo hwclock --rtc /dev/rtc1 -w 1>/dev/null) ]]; then
-	echo "rtc1 was unable to be set"
-fi
-sleep 1;
-
-python3 ~/flash-tv-scripts/python_scripts/set_ext_ds3231_from_dt_now.py
-set_correctly=$?
-
-if [ $set_correctly -eq 1 ]; then
-	zenity --warning --text="Exiting the code since the external RTC time was unable to be set. Please check it and restart the script." --width 500 --height 100 
-	exit 1
-fi
-
 # Disable and stop currently running services
 bash -x ~/flash-tv-scripts/services/stop_services.sh
 
