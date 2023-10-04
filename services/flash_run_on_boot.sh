f1ba302b4ca4dd0b0deec91b9fa84d1b98f454c4
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-10-04 15:49:59 -0500

    rename

--- services/flash_run_on_boot.sh
+++ services/flash_run_on_boot.sh
@@ -8,18 +8,11 @@ export PATH=/usr/local/cuda-11/bin:$PATH
 export MXNET_HOME=/home/$usrName/mxnet
 export PYTHONPATH=$MXNET_HOME/python:$PYTHONPATH
 
-# run time-synchronization commands 
-# if [[ $(sudo hwclock -s 1>/dev/null) ]]; then
-# 	echo "Error occurred when trying to update system time from internal RTC, switching to update from external RTC"
-# 	timedatectl set-ntp 0
-# 	python3 /home/$usrName/flash-tv-scripts/python-scripts/update_timedatectl_from_ext_ds3231.py
-# fi
-
 source /home/$usrName/py38/bin/activate
 
 timedatectl set-ntp 0;
 sleep 5;
-python3 /home/$usrName/flash-tv-scripts/python_scripts/update_system_time.py
+python3 /home/$usrName/flash-tv-scripts/python_scripts/update_system_time_from_RTCs.py
 
 # runs a while loop for the FLASH-TV algorithm only if it doesn't already exist
 while true;
