67256e74fb87a796a9c5c6163109ba88716235d4
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-10-05 13:38:37 -0500

    reorder

--- services/flash_run_on_boot.sh
+++ services/flash_run_on_boot.sh
@@ -16,6 +16,15 @@ timedatectl set-ntp 0;
 sleep 5;
 python3 /home/$usrName/flash-tv-scripts/python_scripts/update_system_time_from_RTCs.py
 
+if [ -e "$index_path" ]; then
+    last_number=$(tail -n 1 "$index_path")
+    new_number=$((last_number + 1))
+else
+    new_number=1
+fi
+
+echo "$new_number" >> "$index_path"
+
 # runs a while loop for the FLASH-TV algorithm only if it doesn't already exist
 while true;
 do	
@@ -25,14 +34,6 @@ then
  	sleep 10;
 	python /home/$usrName/flash-tv-scripts/python_scripts/test_vid_frames_batch_v7_2fps_frminp_newfv_rotate.py $famId /home/$usrName/data/${famId}_data no-save-image $usrName;
  
- 	if [ -e "$index_path" ]; then
-	    last_number=$(tail -n 1 "$index_path")
-	    new_number=$((last_number + 1))
-	else
-	    new_number=1
-	fi
- 
- 	echo "$new_number" >> "$index_path"
 	sleep 30;
 else
 	sleep 30;
