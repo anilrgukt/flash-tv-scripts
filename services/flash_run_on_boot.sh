4a742db8bfd6d87513e9884738b3da3b790879ed
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-10-05 15:26:04 -0500

    removed old index file logic

--- services/flash_run_on_boot.sh
+++ services/flash_run_on_boot.sh
@@ -8,23 +8,12 @@ export PATH=/usr/local/cuda-11/bin:$PATH
 export MXNET_HOME=/home/$usrName/mxnet
 export PYTHONPATH=$MXNET_HOME/python:$PYTHONPATH
 
-index_path="/home/$usrName/data/${famId}_data/${famId}_index.txt"
-
 source /home/$usrName/py38/bin/activate
 python /home/${usrName}/flash-tv-scripts/python_scripts/check_file_events.py $famId /home/${usrName}/data/${famId}_data /home/${usrName}/data/${famId}_flashlog_filesequence.csv &
 
 timedatectl set-ntp 0;
 sleep 5;
 python3 /home/$usrName/flash-tv-scripts/python_scripts/update_system_time_from_RTCs.py
-
-if [ -e "$index_path" ]; then
-    last_number=$(tail -n 1 "$index_path")
-    new_number=$((last_number + 1))
-else
-    new_number=1
-fi
-
-echo "$new_number" >> "$index_path"
 
 # runs a while loop for the FLASH-TV algorithm only if it doesn't already exist
 while true;
