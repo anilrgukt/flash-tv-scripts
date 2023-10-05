50059554aaaf4f6925c5a0d37905c284894cad65
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-10-05 12:54:02 -0500

    added index file

--- services/flash_periodic_restart.sh
+++ services/flash_periodic_restart.sh
@@ -10,6 +10,17 @@ mkdir -p $logFolder
 tegrastats --interval 30000 --logfile /home/$usrName/data/${famId}_data/${famId}_tegrastats.log &
 bash /home/$usrName/flash-tv-scripts/services/flash_check_camera_warnings.sh $famId $usrName &
 
+index_path="/home/$usrName/data/${famId}_data/${famId}_index.txt"
+
+if [ -e "$index_path" ]; then
+    last_number=$(tail -n 1 "$index_path")
+    new_number=$((last_number + 1))
+else
+    new_number=1
+fi
+
+echo "$new_number" >> "$index_path"
+
 source /home/$usrName/py38/bin/activate
 
 timedatectl set-ntp 0;
@@ -24,14 +35,16 @@ do
 	#dt=`date`;
 	dt=$(date +"%d_%b_%Y_%H-%M-%S_%Z")
  	mkdir -p "${logFolder}/varlogs_${dt}"
-	systemctl status flash-run-on-boot.service > "${logFolder}/varlogs_${dt}/log_${dt}.txt"
+  	echo "Index: ${new_number}" >> "${logFolder}/varlogs_${dt}/log_${dt}.txt"
+	systemctl status flash-run-on-boot.service >> "${logFolder}/varlogs_${dt}/log_${dt}.txt"
 	systemctl stop flash-run-on-boot.service
-	systemctl status flash-run-on-boot.service > "${logFolder}/varlogs_${dt}/logend_${dt}.txt"
-	python3 /home/$usrName/flash-tv-scripts/python_scripts/check_all_times.py > "${logFolder}/varlogs_${dt}/timedate_${dt}.txt"
+ 	echo "Index: ${new_number}" >> "${logFolder}/varlogs_${dt}/logend_${dt}.txt"
+	systemctl status flash-run-on-boot.service >> "${logFolder}/varlogs_${dt}/logend_${dt}.txt"
+ 	echo "Index: ${new_number}" >> "${logFolder}/varlogs_${dt}/timedate_${dt}.txt"
+	python3 /home/$usrName/flash-tv-scripts/python_scripts/check_all_times.py >> "${logFolder}/varlogs_${dt}/timedate_${dt}.txt"
 	v4l2-ctl --list-devices > "${logFolder}/varlogs_${dt}/camera_${dt}.txt"
 	
 	pkill -9 -f test_vid_frames_batch_v7_2fps_frminp_newfv_rotate.py
-	
 	
 	mv /home/$usrName/data/${famId}_data/${famId}_flash_logstdout.log /home/$usrName/data/${famId}_data/${famId}_flash_logstderr.log "${logFolder}/varlogs_${dt}"
 	cp /home/$usrName/data/${famId}_data/${famId}_flash_logstdoutp.log /home/$usrName/data/${famId}_data/${famId}_flash_logstderrp.log "${logFolder}/varlogs_${dt}"
