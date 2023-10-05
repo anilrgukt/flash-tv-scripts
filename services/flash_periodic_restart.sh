968ad8e836b99d632cdfff17f52dedbaf0d13f10
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-10-05 15:26:23 -0500

    altered old index file logic to be reboot index file

--- services/flash_periodic_restart.sh
+++ services/flash_periodic_restart.sh
@@ -7,7 +7,7 @@ export usrName=flashsysXXX
 logFolder=/home/$usrName/data/${famId}_data/logs
 mkdir -p $logFolder
 
-index_path="/home/$usrName/data/${famId}_data/${famId}_index.txt"
+reboot_index_path="/home/$usrName/data/${famId}_data/${famId}_reboot_index.txt"
 
 tegrastats --interval 30000 --logfile /home/$usrName/data/${famId}_data/${famId}_tegrastats.log &
 bash /home/$usrName/flash-tv-scripts/services/flash_check_camera_warnings.sh $famId $usrName &
@@ -22,21 +22,23 @@ do
 	#DOW=$(date +"%d_%b_%Y_%H-%M-%S_%Z")
 	#dt=`date`;
 	
-	if [ -e "$index_path" ]; then
-	    last_number=$(tail -n 1 "$index_path")
+	if [ -e "$reboot_index_path" ]; then
+	    last_number=$(tail -n 1 "$reboot_index_path")
+     	    new_number=$((last_number + 1))
 	else
-	    last_number=1
-	    echo "$last_number" >> "$index_path"
+	    new_number=1
 	fi
+ 
+ 	echo "$new_number" >> "$reboot_index_path"
  
 	dt=$(date +"%d_%b_%Y_%H-%M-%S_%Z")
  	mkdir -p "${logFolder}/varlogs_${dt}"
-  	echo "Index: ${last_number}" >> "${logFolder}/varlogs_${dt}/log_${dt}.txt"
+  	echo "Backup Index: ${new_number}" >> "${logFolder}/varlogs_${dt}/log_${dt}.txt"
 	systemctl status flash-run-on-boot.service >> "${logFolder}/varlogs_${dt}/log_${dt}.txt"
 	systemctl stop flash-run-on-boot.service
- 	echo "Index: ${last_number}" >> "${logFolder}/varlogs_${dt}/logend_${dt}.txt"
+ 	echo "Backup Index: ${new_number}" >> "${logFolder}/varlogs_${dt}/logend_${dt}.txt"
 	systemctl status flash-run-on-boot.service >> "${logFolder}/varlogs_${dt}/logend_${dt}.txt"
- 	echo "Index: ${last_number}" >> "${logFolder}/varlogs_${dt}/timedate_${dt}.txt"
+ 	echo "Backup Index: ${new_number}" >> "${logFolder}/varlogs_${dt}/timedate_${dt}.txt"
 	python3 /home/$usrName/flash-tv-scripts/python_scripts/check_all_times.py >> "${logFolder}/varlogs_${dt}/timedate_${dt}.txt"
 	v4l2-ctl --list-devices > "${logFolder}/varlogs_${dt}/camera_${dt}.txt"
 	
