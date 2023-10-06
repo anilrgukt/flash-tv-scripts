a07e15f28e12c017e7ab842e53c17e30493b5b57
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-10-06 12:21:46 -0500

    QoL changes

--- participant_change.sh
+++ participant_change.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
 # MUST DELETE AND RECLONE the flash-tv-scripts folder BEFORE RUNNING THIS OR IT WILL NOT WORK PROPERLY
 
-# Set to exit on non-zero error code
-set -e
+if [ ! -d ~/.homeassistant ]; then
+	zenity --warning --text="Exiting the code since Home Assistant has not been set up.\n\nPlease set up Home Assistant before running this script." --width 500 --height 100
+	exit 1
+fi
 
 # Setting up plug ID for Home Assistant config file
-read -p 'Enter plug ID (4 characters lowercase) or YYYY (uppercase) if the plug is not yet ready:' plugID
+plugID=$(zenity --entry --width 500 --height 100 --text="Enter plug ID (4 characters lowercase) or YYYY (uppercase) if the plug is not ready:")
 
-zenity --question --title="Verify Plug ID" --width 500 --height 100 --text="Please verify the following details\nPlug ID: $plugID" --no-wrap
+zenity --question --title="Verify Plug ID" --text="Please verify the following details:\n\nPlug ID: $plugID" --width 500 --height 100 
+
 user_resp=$?
 
 if [ $user_resp -eq 1 ]; then
-	#echo "Exitted the code $user_resp"
-	zenity --warning --text="Exiting the code since data details are not correct. Please modify them and restart the script."
+
+	zenity --warning --text="Exiting the code since the plug ID was not entered correctly according to the user.\n\nPlease restart the script to try again." --width 500 --height 100
 	exit 1
+
 fi
+
+# Set to exit on non-zero error code
+set -e
 
 sed -i "s/YYYY/$plugID/g" ~/flash-tv-scripts/install_scripts/configuration.yaml
 
 bash -x ~/flash-tv-scripts/setup_scripts/id_setup.sh
+sleep 1;
+
 bash -x ~/flash-tv-scripts/setup_scripts/service_setup.sh
+sleep 1;
+
 bash -x ~/flash-tv-scripts/setup_scripts/RTC_setup.sh
+sleep 1;
 
 # Copying the config file to the Home Assistant directory
 cp ~/flash-tv-scripts/install_scripts/configuration.yaml ~/.homeassistant
