51ba59f1bec25f9a82ccdbb6b789d5656e8b59b3
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-10-06 12:18:16 -0500

    added logs removed scrolling

--- services/restart_services.sh
+++ services/restart_services.sh
@@ -6,3 +6,7 @@ sleep 1;
 sudo systemctl restart flash-run-on-boot.service
 sleep 1;
 sudo systemctl restart homeassistant-run-on-boot.service
+sleep 1;
+
+#Display the status of all services
+sudo systemctl status --no-pager flash-periodic-restart.service flash-run-on-boot.service homeassistant-run-on-boot.service
