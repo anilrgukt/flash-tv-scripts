d40859905b0117dae2829083056772176674b8ef
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-10-13 19:57:45 -0500

    changed backup USB mount path variable

--- setup_scripts/USB_backup_setup.sh
+++ setup_scripts/USB_backup_setup.sh
@@ -19,10 +19,12 @@ if [ ! `lsusb | grep -q "SanDisk Corp. Ultra Fit"` ]; then
 	# Enable automounting of the USB on boot (disabled by default)
  	FSTAB=/etc/fstab
 
- 	backup_usb_mount_line="UUID=${backup_usb_uuid} /media/flashsysXXX/${backup_usb_uuid} auto uid=${UID},gid=${UID} 0 0"
+  	backup_usb_mount_path=/home/media/${backup_usb_uuid}
 
- 	grep -q '.*UUID=.* /media/flashsysXXX.* auto uid=.*,gid=.* 0 0.*' "${FSTAB}" || echo "${backup_usb_mount_line}" | sudo tee -a "${FSTAB}"
-  	sudo sed -i "s@.*UUID=.* /media/flashsysXXX.* auto uid=.*,gid=.* 0 0.*@${backup_usb_mount_line}@" "${FSTAB}"
+ 	backup_usb_fstab_line="UUID=${backup_usb_uuid} /home/media/${backup_usb_uuid} auto uid=${UID},gid=${UID} 0 0"
+
+ 	grep -q '.*UUID=.* /home/media/.* auto uid=.*,gid=.* 0 0.*' "${FSTAB}" || echo "${backup_usb_fstab_line}" | sudo tee -a "${FSTAB}"
+  	sudo sed -i "s@.*UUID=.* /home/media/.* auto uid=.*,gid=.* 0 0.*@${backup_usb_fstab_line}@" "${FSTAB}"
  
  	sudo sed -i /etc/fstab -e 's/noauto//' -e 's/ ,,/ /' -e 's/ ,/ /' -e 's/,,/,/' -e 's/, / /'
  
@@ -56,9 +58,9 @@ if [ ! `lsusb | grep -q "SanDisk Corp. Ultra Fit"` ]; then
 	sed -i "s@.*BORG_PASSPHRASE.*@${BORG_PASSPHRASE_EXPORT}@" "${BASHRC}"
 
 	# Export and save borg repo path
-	export BORG_REPO=${backup_usb_path}/USB_Backup_Data_flashsysXXX
+	export BORG_REPO=${backup_usb_mount_path}/USB_Backup_Data_flashsysXXX
 	
-	BORG_REPO_EXPORT="export BORG_REPO=${backup_usb_path}/USB_Backup_Data_flashsysXXX"
+	BORG_REPO_EXPORT="export BORG_REPO=${backup_usb_mount_path}/USB_Backup_Data_flashsysXXX"
 	
 	grep -q '.*BORG_REPO.*' "${BASHRC}" || echo "${BORG_REPO_EXPORT}" >> "${BASHRC}"
 	sed -i "s@.*BORG_REPO.*@${BORG_REPO_EXPORT}@" "${BASHRC}"
@@ -67,7 +69,7 @@ if [ ! `lsusb | grep -q "SanDisk Corp. Ultra Fit"` ]; then
 	borg init -v --encryption=repokey
 
 	# Export borg encryption keys to multiple places for backup
-	borg key export --paper :: > ${backup_usb_path}/borg-encrypted-key-backup-flashsysXXX.txt
+	borg key export --paper :: > ${backup_usb_mount_path}/borg-encrypted-key-backup-flashsysXXX.txt
 	borg key export --paper :: > /home/flashsysXXX/borg-encrypted-key-backup-flashsysXXX.txt
 	borg key export --paper :: > /home/flashsysXXX/flash-tv-scripts/setup_scripts/borg-encrypted-key-backup-flashsysXXX.txt
 
