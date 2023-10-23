e37046de14ef6b35565aea1729d7905f9a3a8146
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-10-23 10:43:01 -0500

    rename

--- services/flash_periodic_restart.sh
+++ services/flash_periodic_restart.sh
@@ -39,12 +39,12 @@ do
 	dt=$(date +"%d_%b_%Y_%H-%M-%S_%Z")
 	
 	mkdir -p "${logFolder}/varlogs_${dt}"
-	echo "Reboot Index: ${new_number}" >> "${logFolder}/varlogs_${dt}/log_${dt}.txt"
+	echo "Reboot Index: ${new_index}" >> "${logFolder}/varlogs_${dt}/log_${dt}.txt"
 	systemctl status flash-run-on-boot.service >> "${logFolder}/varlogs_${dt}/log_${dt}.txt"
 	systemctl stop flash-run-on-boot.service
-	echo "Reboot Index: ${new_number}" >> "${logFolder}/varlogs_${dt}/logend_${dt}.txt"
+	echo "Reboot Index: ${new_index}" >> "${logFolder}/varlogs_${dt}/logend_${dt}.txt"
 	systemctl status flash-run-on-boot.service >> "${logFolder}/varlogs_${dt}/logend_${dt}.txt"
-	echo "Reboot Index: ${new_number}" >> "${logFolder}/varlogs_${dt}/timedate_${dt}.txt"
+	echo "Reboot Index: ${new_index}" >> "${logFolder}/varlogs_${dt}/timedate_${dt}.txt"
 	python3 /home/$usrName/flash-tv-scripts/python_scripts/check_all_times.py >> "${logFolder}/varlogs_${dt}/timedate_${dt}.txt"
 	v4l2-ctl --list-devices > "${logFolder}/varlogs_${dt}/camera_${dt}.txt"
 	
