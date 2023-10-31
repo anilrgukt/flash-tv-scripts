7c2e3f12619f7cf945aa2e42d6cad6f1594d215c
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-10-31 15:08:27 -0500

    revert

--- python_scripts/check_all_times.py
+++ python_scripts/check_all_times.py
@@ -10,7 +10,7 @@ I2C_BUS_NUMBER = 1  # Replace with the actual bus number if different
 
 def stderr_print(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
-
+    
 def read_rtc_data(bus):
     return bus.read_i2c_block_data(RTC_ADDRESS, 0, 8)
 
@@ -18,37 +18,57 @@ def hex_rtc_data(bus):
     return [hex(x) for x in read_rtc_data(bus)]
 
 def dec_rtc_data(hex_data):
-    return [int(x.replace("0x", ""), 16) for x in hex_data]
+    return [int(x.replace("0x", "")) for x in hex_data]
 
 def convert_rtc_format_to_timedatectl_format(bus):
     rtc_data = dec_rtc_data(hex_rtc_data(bus))
     return f"20{rtc_data[6]:02}-{rtc_data[5]:02}-{rtc_data[4]:02} {rtc_data[2]:02}:{rtc_data[1]:02}:{rtc_data[0]:02}"
 
-def run_command(command, error_message, success_message=None):
-    try:
-        result = subprocess.check_output(command)
-        if success_message:
-            print(success_message)
-        return result.decode('utf-8')
-    except Exception as e:
-        stderr_print(f"{error_message}: {str(e)}")
-        return None
-
-def check_and_print(command, success_message, error_message):
-    result = run_command(command, error_message, success_message)
-    if result:
-        print(result)
-
 def check_times():
+
     bus = SMBus(I2C_BUS_NUMBER)
     
-    check_and_print(["timedatectl"], None, "Unable to run timedatectl for system time info")
-    check_and_print(['sudo', 'hwclock', '-r'], "Time from internal RTC rtc0 (PSEQ_RTC, being used) is: {result}", "Unable to obtain time from internal RTC rtc0 (PSEQ_RTC, being used) for validation")
-    check_and_print(['timedatectl'], "Time from external RTC (DS3231) is: {result}", "Unable to obtain time from external RTC for validation")
-    check_and_print(['sudo', 'hwclock', '--rtc', '/dev/rtc1'], "Time from internal RTC rtc1 (tegra-RTC, not being used) is: {result}", "Unable to obtain time from internal RTC rtc1 (tegra-RTC, not being used)")
+    try:
+        timedatectl = subprocess.check_output(["timedatectl"])
+        for line in timedatectl.splitlines():
+            print(line.strip().decode('utf-8'))
+        #TIMEDATECTL_SUCCESSFUL = True
+    except:
+        stderr_print(traceback.format_exc())
+        stderr_print("Warning: unable to run timedatectl for system time info")
+        pass
+        #TIMEDATECTL_SUCCESSFUL = False
+    
+    try:
+        print(f"Time from internal RTC rtc0 (PSEQ_RTC, being used) is: {subprocess.check_output(['sudo', 'hwclock', '-r']).strip().decode('utf-8')}")
+        #INTERNAL_RTC0_READ_SUCCESSFUL = True
+    except:
+        stderr_print(traceback.format_exc())
+        stderr_print("Warning: Unable to obtain time from internal RTC rtc0 (PSEQ_RTC, being used) for validation")
+        pass
+        #INTERNAL_RTC0_READ_SUCCESSFUL = False
+    
+    try:
+        print(f"Time from external RTC (DS3231) is: {convert_rtc_format_to_timedatectl_format(bus)}")
+        bus.close()
+    except:
+        stderr_print(traceback.format_exc())
+        stderr_print("Warning: Unable to obtain time from external RTC for validation")
+        pass
+        
+    try:
+        print(f"Time from internal RTC rtc1 (tegra-RTC, not being used) is: {subprocess.check_output(['sudo', 'hwclock', '--rtc', '/dev/rtc1']).decode('utf-8')}")
+        #INTERNAL_RTC1_READ_SUCCESSFUL = True
+    except:
+        stderr_print(traceback.format_exc())
+        stderr_print("Info: Unable to obtain time from internal RTC rtc1 (tegra-RTC, not being used)")
+        pass
+        #INTERNAL_RTC1_READ_SUCCESSFUL = False
     
     if bus:
         bus.close()
+        
+    return
 
-if __name__ == "__main":
+if __name__ == "__main__":
     check_times()
