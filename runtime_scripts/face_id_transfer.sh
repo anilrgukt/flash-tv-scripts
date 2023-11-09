d35d2aeef4a705ef581ea94bf58a6172f4f69e31
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-11-09 14:40:12 -0600

    added command line option

--- runtime_scripts/face_id_transfer.sh
+++ runtime_scripts/face_id_transfer.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-read -p 'Enter the ID of the device you are transferring faces FROM (3 digits):' olddeviceID
-read -p 'Enter the ID of the device you are transferring faces TO (3 digits):' newdeviceID
+if [ "$#" -lt 2 ] && [ "$#" -ne 0 ]; then
+
+  echo "Command Line Usage: $0 (ID of Device You are Transferring Faces FROM) (ID of Device You are Transferring Faces TO)"
+  read -p 'Enter the ID of the device you are transferring faces FROM (3 digits):' olddeviceID
+  read -p 'Enter the ID of the device you are transferring faces TO (3 digits):' newdeviceID
+  
+else
+
+  olddeviceID=$1
+  newdeviceID=$2
+  
+fi
 
 facefolder=`ls ~/data/123XXX_data/ | grep faces`
 
