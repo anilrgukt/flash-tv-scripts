c46f0ef441acf5748b71a3e4ec2247e79c4582c6
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-11-09 15:55:39 -0600

    rename

--- full_initial_configuration.sh
+++ full_initial_configuration.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Run all setup files
-bash -x ~/flash-tv-scripts/setup_scripts/id_setup.sh
+bash -x ~/flash-tv-scripts/setup_scripts/ID_setup.sh
 bash -x ~/flash-tv-scripts/setup_scripts/file_setup.sh
 bash -x ~/flash-tv-scripts/setup_scripts/service_setup.sh
 bash -x ~/flash-tv-scripts/install_scripts/full_install.sh
