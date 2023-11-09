ca0cfbcfef1a6734997fff40a8ad4a6b0b65bc41
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-11-09 16:09:44 -0600

    added command line option to skip checking for testing automation

--- participant_change.sh
+++ participant_change.sh
@@ -6,16 +6,28 @@ if [ ! -d ~/.homeassistant ]; then
 	exit 1
 fi
 
+skip_checking=0
+
 if [ "$#" -ne 3 ]; then
 
-	echo "Command Line Usage: $0 (plugID) (deviceID) (familyID)"
-	
-	# Setting up plug ID for Home Assistant config file
-	plugID=$(zenity --entry --width 500 --height 100 --text="Enter plug ID (4 characters lowercase) or YYYY (uppercase) if the plug is not ready:")
-	
-	deviceID=$(zenity --entry --width 500 --height 100 --text="Enter FLASH device ID (3 digits):")
-	
-	familyID=$(zenity --entry --width 500 --height 100 --text="Enter family ID (3 digits for Study 4, P1-1[3 digits no brackets] for TECH):")
+	if [ "$#" -eq 4 ]; then
+
+		plugID=$1
+		deviceID=$2
+		familyID=$3
+  		skip_checking=$4
+
+ 	else
+
+		echo "Command Line Usage: $0 (plugID) (deviceID) (familyID)"
+		
+		# Setting up plug ID for Home Assistant config file
+		plugID=$(zenity --entry --width 500 --height 100 --text="Enter plug ID (4 characters lowercase) or YYYY (uppercase) if the plug is not ready:")
+		
+		deviceID=$(zenity --entry --width 500 --height 100 --text="Enter FLASH device ID (3 digits):")
+		
+		familyID=$(zenity --entry --width 500 --height 100 --text="Enter family ID (3 digits for Study 4, P1-1[3 digits no brackets] for TECH):")
+ 	fi
    
 else
 
@@ -25,12 +37,15 @@ else
   
 fi
 
-zenity --question --title="Verify Plug ID, Device ID, and Family ID" --width 500 --height 100 --text="Please verify the following details\n\nPlug ID: $plugID\nFamily ID: ${familyID}\nDevice ID: ${deviceID}" --no-wrap
-user_resp=$?
+if [ $skip_checking -ne 1]; then
 
-if [ ${user_resp} -eq 1 ]; then
-	zenity --warning --text="Exiting the code since the plug ID, device ID, and/or family ID were not entered correctly according to the user. Please restart the script to try again." --width 500 --height 100
-	exit 1
+	zenity --question --title="Verify Plug ID, Device ID, and Family ID" --width 500 --height 100 --text="Please verify the following details\n\nPlug ID: $plugID\nFamily ID: ${familyID}\nDevice ID: ${deviceID}" --no-wrap
+	user_resp=$?
+	
+	if [ ${user_resp} -eq 1 ]; then
+		zenity --warning --text="Exiting the code since the plug ID, device ID, and/or family ID were not entered correctly according to the user. Please restart the script to try again." --width 500 --height 100
+		exit 1
+	fi
 fi
 
 # Set to exit on non-zero error code
@@ -38,7 +53,7 @@ set -e
 
 sed -i "s/YYYY/$plugID/g" ~/flash-tv-scripts/install_scripts/configuration.yaml
 
-bash -x ~/flash-tv-scripts/setup_scripts/ID_setup.sh $deviceID $familyID
+bash -x ~/flash-tv-scripts/setup_scripts/ID_setup.sh $deviceID $familyID 1
 sleep 1;
 
 bash -x ~/flash-tv-scripts/setup_scripts/USB_backup_setup.sh
