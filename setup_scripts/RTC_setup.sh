e23c61f0ff261185feb51e03a3a88c22b2b0d687
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-11-09 13:17:54 -0600

    added start date file path as input argument to Python script

--- setup_scripts/RTC_setup.sh
+++ setup_scripts/RTC_setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+export username=flashsysXXX
 export famId=123XXX
 
-# Set up rtc0 (PSEQ_RTC) as the main RTC
+# Set up rtc0 (PSEQ_RTC) as the main interal RTC instead of rtc1 (tegra-RTC)
 sudo sed -i 's/ATTR{hctosys}=="1"/ATTR{hctosys}=="0"/g' /lib/udev/rules.d/50-udev-default.rules
 
 source ~/py38/bin/activate
@@ -19,7 +20,7 @@ sleep 1;
 sudo hwclock --rtc /dev/rtc1 -w && echo "Internal RTC rtc1 (tegra-RTC, not being used) was set" || "Internal RTC rtc1 (tegra-RTC, not being used) was unable to be set"
 sleep 1;
 
-python3 ~/flash-tv-scripts/python_scripts/set_ext_RTC_and_save_start_date.py ${famId}
+python3 ~/flash-tv-scripts/python_scripts/set_ext_RTC_and_save_start_date.py "/home/${username}/data/${famId}_data/${famId}_start_date.txt"
 set_correctly=$?
 
 if [ $set_correctly -eq 1 ]; then
