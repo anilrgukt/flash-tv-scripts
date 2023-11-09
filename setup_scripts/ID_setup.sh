af779279c2591cb5a0dc5089f05cc39bace24534
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-11-09 15:48:31 -0600

    added command line option

--- setup_scripts/ID_setup.sh
+++ setup_scripts/ID_setup.sh
@@ -4,11 +4,21 @@
 rm -r ~/flash-tv-scripts/video_capture_scripts
 rm -r ~/flash-tv-scripts/internal_testing
 
+if [ "$#" -ne 2 ]; then
 
-deviceID=$(zenity --entry --width 500 --height 100 --text="Enter FLASH device ID (3 digits):")
+   echo "Command Line Usage: $0 (deviceID) (familyID)"
+   
+   deviceID=$(zenity --entry --width 500 --height 100 --text="Enter FLASH device ID (3 digits):")
+   
+   familyID=$(zenity --entry --width 500 --height 100 --text="Enter family ID (3 digits for Study 4, P1-1[3 digits no brackets] for TECH):")
 
-familyID=$(zenity --entry --width 500 --height 100 --text="Enter family ID (3 digits for Study 4, P1-1[3 digits no brackets] for TECH):")
-
+else
+ 
+  deviceID=$1
+  familyID=$2
+  
+fi
+  
 zenity --question --title="Verify Device and Family ID" --width 500 --height 100 --text="Please verify the following details\nFamily ID: ${familyID}\nDevice ID: ${deviceID}" --no-wrap
 user_resp=$?
 
