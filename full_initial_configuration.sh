60561e1262131e5b8b4a9ef9a1e16b1f1d88358b
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2023-11-16 17:42:47 -0600

    added USB backup setup

--- full_initial_configuration.sh
+++ full_initial_configuration.sh
@@ -7,5 +7,6 @@ set -e
 bash -x ~/flash-tv-scripts/setup_scripts/ID_setup.sh
 bash -x ~/flash-tv-scripts/setup_scripts/file_setup.sh
 bash -x ~/flash-tv-scripts/setup_scripts/service_setup.sh
+bash -x ~/flash-tv-scripts/setup_scripts/USB_backup_setup.sh
 bash -x ~/flash-tv-scripts/install_scripts/full_install.sh
 bash -x ~/flash-tv-scripts/setup_scripts/RTC_setup.sh
