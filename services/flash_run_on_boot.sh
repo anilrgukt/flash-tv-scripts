8fa29a71ca73c9b04aa84810a8ff6d247118c520
anilrgukt <anil.rgukt@gmail.com>
2024-06-11 16:42:37 -0500

    Update flash_run_on_boot.sh
    format correction

--- services/flash_run_on_boot.sh
+++ services/flash_run_on_boot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export famId=P1-3344008
-export usrName=flashsys008
+export famId=123XXX
+export usrName=flashsysXXX
 
 export LD_LIBRARY_PATH=/home/$usrName/mxnet/lib:$LD_LIBRARY_PATH
 export PATH=/usr/local/cuda-11/bin:$PATH
