4b1e9ead28a42c75ee4763ddf6c4762bcc81d7a0
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2024-06-11 17:04:07 -0500

    Add files via upload

--- setup_scripts/USB_backup_setup.sh
+++ setup_scripts/USB_backup_setup.sh
@@ -4,7 +4,7 @@ source ~/py38/bin/activate
 
 skip_checking=$1
 
-if borg list > /dev/null 2>&1 && borg list | grep -Pq "\d{3}008" && [ "$skip_checking" -eq 1 ]; then
+if borg list > /dev/null 2>&1 && borg list | grep -Pq "\d{3}XXX" && [ "$skip_checking" -eq 1 ]; then
 	echo "Skipping USB backup setup since it was already set up"
  	exit 0
 fi
@@ -28,12 +28,12 @@ if [ ! `lsusb | grep -q "SanDisk Corp. Ultra Fit"` ]; then
 	# Enable automounting of the USB on boot (disabled by default)
  	FSTAB=/etc/fstab
 
-  	backup_usb_mount_path=/media/flashsys008/${backup_usb_uuid}
+  	backup_usb_mount_path=/media/flashsysXXX/${backup_usb_uuid}
 
- 	backup_usb_fstab_line="UUID=${backup_usb_uuid} /media/flashsys008/${backup_usb_uuid} auto uid=${UID},gid=${UID} 0 0"
+ 	backup_usb_fstab_line="UUID=${backup_usb_uuid} /media/flashsysXXX/${backup_usb_uuid} auto uid=${UID},gid=${UID} 0 0"
 
- 	grep -q '.*UUID=.* /media/flashsys008/.* auto uid=.*,gid=.* 0 0.*' "${FSTAB}" || echo "${backup_usb_fstab_line}" | sudo tee -a "${FSTAB}"
-  	sudo sed -i "s@.*UUID=.* /media/flashsys008/.* auto uid=.*,gid=.* 0 0.*@${backup_usb_fstab_line}@" "${FSTAB}"
+ 	grep -q '.*UUID=.* /media/flashsysXXX/.* auto uid=.*,gid=.* 0 0.*' "${FSTAB}" || echo "${backup_usb_fstab_line}" | sudo tee -a "${FSTAB}"
+  	sudo sed -i "s@.*UUID=.* /media/flashsysXXX/.* auto uid=.*,gid=.* 0 0.*@${backup_usb_fstab_line}@" "${FSTAB}"
  
  	sudo sed -i /etc/fstab -e 's/noauto//' -e 's/ ,,/ /' -e 's/ ,/ /' -e 's/,,/,/' -e 's/, / /'
  
@@ -43,7 +43,7 @@ if [ ! `lsusb | grep -q "SanDisk Corp. Ultra Fit"` ]; then
 	zenity --entry --hide-text --width 500 --height 100 --text="Enter USB Backup Password:" > "${temp_file}"
 	
 	# Send password to be checked and encoded using cryptography modules in Python
-	encoded_password=`python3 /home/flashsys008/flash-tv-scripts/python_scripts/check_and_encode_password.py "${temp_file}"`
+	encoded_password=`python3 /home/flashsysXXX/flash-tv-scripts/python_scripts/check_and_encode_password.py "${temp_file}"`
 	exit_code=$?
 	
 	# Overwrite and destroy temp file
@@ -54,7 +54,7 @@ if [ ! `lsusb | grep -q "SanDisk Corp. Ultra Fit"` ]; then
 		exit 1
 	fi
 	
-	BASHRC=/home/flashsys008/.bashrc
+	BASHRC=/home/flashsysXXX/.bashrc
 
 	# Export and save encoded password as borg passphrase
 	export BORG_PASSPHRASE="${encoded_password}"
@@ -65,9 +65,9 @@ if [ ! `lsusb | grep -q "SanDisk Corp. Ultra Fit"` ]; then
 	sed -i "s@.*BORG_PASSPHRASE.*@${BORG_PASSPHRASE_EXPORT_LINE}@" "${BASHRC}"
 
 	# Export and save borg repo path
-	export BORG_REPO=${backup_usb_mount_path}/USB_Backup_Data_flashsys008
+	export BORG_REPO=${backup_usb_mount_path}/USB_Backup_Data_flashsysXXX
 	
-	BORG_REPO_EXPORT_LINE="export BORG_REPO='${backup_usb_mount_path}/USB_Backup_Data_flashsys008'"
+	BORG_REPO_EXPORT_LINE="export BORG_REPO='${backup_usb_mount_path}/USB_Backup_Data_flashsysXXX'"
 	
 	grep -q '.*BORG_REPO.*' "${BASHRC}" || echo "${BORG_REPO_EXPORT_LINE}" >> "${BASHRC}"
 	sed -i "s@.*BORG_REPO.*@${BORG_REPO_EXPORT_LINE}@" "${BASHRC}"
@@ -79,9 +79,9 @@ if [ ! `lsusb | grep -q "SanDisk Corp. Ultra Fit"` ]; then
 	borg init -v --encryption=repokey
 
 	# Export borg encryption keys to multiple places for backup
-	borg key export --paper :: > ${backup_usb_mount_path}/borg-encrypted-key-backup-flashsys008.txt
-	borg key export --paper :: > /home/flashsys008/borg-encrypted-key-backup-flashsys008.txt
-	borg key export --paper :: > /home/flashsys008/flash-tv-scripts/setup_scripts/borg-encrypted-key-backup-flashsys008.txt
+	borg key export --paper :: > ${backup_usb_mount_path}/borg-encrypted-key-backup-flashsysXXX.txt
+	borg key export --paper :: > /home/flashsysXXX/borg-encrypted-key-backup-flashsysXXX.txt
+	borg key export --paper :: > /home/flashsysXXX/flash-tv-scripts/setup_scripts/borg-encrypted-key-backup-flashsysXXX.txt
 
 else
 	zenity --warning --width 500 --height 100 --text="Exiting the code since the backup USB was not detected in lsusb.\nPlease reconnect the backup USB and try again."
