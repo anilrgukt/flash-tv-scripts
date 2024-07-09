7e2cc55548785e6cbe86fc5e09e0ec69ba4fc60a
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2024-07-09 17:42:10 -0500

    made brackets consistent

--- services/flash_run_on_boot.sh
+++ services/flash_run_on_boot.sh
@@ -8,12 +8,12 @@ export PATH=/usr/local/cuda-11/bin:$PATH
 export MXNET_HOME=/home/$usrName/mxnet
 export PYTHONPATH=$MXNET_HOME/python:$PYTHONPATH
 
-source /home/$usrName/py38/bin/activate
+source /home/${usrName}/py38/bin/activate
 python /home/${usrName}/flash-tv-scripts/python_scripts/check_file_events.py $famId /home/${usrName}/data/${famId}_data /home/${usrName}/data/${famId}_data/${famId}_flashlog_filesequence.csv &
 
 timedatectl set-ntp 0;
 sleep 1;
-python3 /home/$usrName/flash-tv-scripts/python_scripts/update_system_time_from_RTCs.py "/home/$usrName/data/${famId}_data/${famId}_start_date.txt"
+python3 /home/${usrName}/flash-tv-scripts/python_scripts/update_system_time_from_RTCs.py /home/${usrName}/data/${famId}_data/${famId}_start_date.txt
 
 suffix="${usrName: -2}"
 
