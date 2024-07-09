75d382f58fb2d3304a963c6023e16d32c228cb9c
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2024-07-09 16:12:36 -0500

    fix syntax errors

--- services/flash_run_on_boot.sh
+++ services/flash_run_on_boot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-export famId=123XXX
-export usrName=flashsysXXX
+export famId=P1-1234004
+export usrName=flashsys004
 
 export LD_LIBRARY_PATH=/home/$usrName/mxnet/lib:$LD_LIBRARY_PATH
 export PATH=/usr/local/cuda-11/bin:$PATH
@@ -15,17 +15,17 @@ timedatectl set-ntp 0;
 sleep 1;
 python3 /home/$usrName/flash-tv-scripts/python_scripts/update_system_time_from_RTCs.py "/home/$usrName/data/${famId}_data/${famId}_start_date.txt"
 
-suffix="${usrName: -2}" ​
-if [ ! $(sudo ip addr add 10.0.0.${suffix}/24 dev eth0 | grep "RTNETLINK answers: File exists") ]​
-then​
-	echo "Info: IP was possibly already assigned in this reboot cycle as the 'RTNETLINK answers: File exists' message was detected.";​
+suffix="${usrName: -2}"
+
+if [ ! $(sudo ip addr add 10.0.0.${suffix}/24 dev eth0 | grep "RTNETLINK answers: File exists") ]; then
+	echo "Info: IP was possibly already assigned in this reboot cycle as the 'RTNETLINK answers: File exists' message was detected.";​	
 fi
 
-# runs a while loop for the FLASH-TV algorithm only if it doesnt already exist
+# runs a while loop for the FLASH-TV algorithm only if it doesn't already exist
+
 while true;
 do	
-if ! [ "`pgrep -af run_flash_data_collection.py`" ]
-then
+if ! [ "`pgrep -af run_flash_data_collection.py`" ]; then
 	free -m && sync && echo 1 > /proc/sys/vm/drop_caches && free -m;
 
  	sleep 1;
@@ -35,6 +35,5 @@ else
 	sleep 30;
 fi
 done
-
 
 
