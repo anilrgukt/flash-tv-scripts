a58b5ddca8c63af0fb02c921c6712a46d83cf7ae
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2024-10-16 15:17:55 -0500

    update homeassistant version to latest

--- install_scripts/homeassistant_install.sh
+++ install_scripts/homeassistant_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #see homeassistant_setup.txt for how to initialize homeassistant after this
 
-#for Python 3.10 easy install
+#for Python 3.12 easy install
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get update
 
 #install homeassistant dependencies
-sudo apt-get install -y python3.10 python3.10-dev python3.10-venv python3-pip bluez libffi-dev libssl-dev libjpeg-dev zlib1g-dev autoconf build-essential libopenjp2-7 libtiff5 libturbojpeg0-dev tzdata ffmpeg liblapack3 liblapack-dev libatlas-base-dev
+sudo apt-get install -y python3.12 python3.12-dev python3.12-venv python3-pip bluez libffi-dev libssl-dev libjpeg-dev zlib1g-dev autoconf build-essential libopenjp2-7 libtiff5 libturbojpeg0-dev tzdata ffmpeg liblapack3 liblapack-dev libatlas-base-dev
 
 #set up homeassistant folder
 cd /home/$USER
@@ -15,15 +15,15 @@ mkdir ha
 #go to homeassistant directory
 cd ha
 
-#create Python 3.10 virtual environment if it doesn't already exist and activate it 
+#create Python 3.12 virtual environment if it doesn't already exist and activate it 
 if ! ls | grep -q "bin"
 then 
-	python3.10 -m venv .
+	python3.12 -m venv .
 fi
 source bin/activate
 
 #install Python dependencies and then homeassistant
 pip install --upgrade pip
 pip3 install wheel
-pip3 install homeassistant==2023.7.3
+pip3 install homeassistant==2024.10.2
 pip3 install git+https://github.com/boto/botocore
