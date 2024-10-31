1a182090d00ffe09abb1fbff13f050af914893aa
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2024-10-31 16:31:38 -0500

    removed homeassistant service due to change to docker

--- services/restart_services.sh
+++ services/restart_services.sh
@@ -5,8 +5,6 @@ sudo systemctl restart flash-periodic-restart.service
 sleep 1;
 sudo systemctl restart flash-run-on-boot.service
 sleep 1;
-sudo systemctl restart homeassistant-run-on-boot.service
-sleep 1;
 
 #Display the status of all services
-sudo systemctl status --no-pager flash-periodic-restart.service flash-run-on-boot.service homeassistant-run-on-boot.service
+sudo systemctl status --no-pager flash-periodic-restart.service flash-run-on-boot.service
