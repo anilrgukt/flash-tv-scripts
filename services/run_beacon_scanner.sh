cc0d11e0aa4822a2dc82db26b339072bacc11d62
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2024-11-15 13:50:50 -0600

    typo

--- services/run_beacon_scanner.sh
+++ services/run_beacon_scanner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Path to your compiled C program
-PROGRAM_PATH="~/flash-tv-scripts/services/scanner.c
+PROGRAM_PATH="~/flash-tv-scripts/services/scanner.c"
 
 while true; do
   # Run the program
