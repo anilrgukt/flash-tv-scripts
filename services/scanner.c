578cc51c1e88d438547778e06b945d3203ce84e4
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2024-11-15 13:52:38 -0600

    decrease duration until timeout from 60 seconds to 40 seconds

--- services/scanner.c
+++ services/scanner.c
@@ -204,9 +204,9 @@ int main()
 	int len;
 	int count = 0;
 
-	const int timeout = 60;
+	const int timeout = 40;
 	const int reset_timeout = 1; // wether to reset the timer on a received scan event (continuous scanning)
-	const int max_count = 10000;
+	// const int max_count = 10000;
 
 	// Install a signal handler so that we can set the exit code and clean up
 	if ( signal( SIGALRM, signal_handler ) == SIG_ERR )
