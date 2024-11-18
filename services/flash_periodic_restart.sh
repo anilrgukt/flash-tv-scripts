f5b5ae5c1b39e9140f88a6a999ba927fd7dd18ca
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2024-11-18 14:24:35 -0600

    improved formatting and running of Bluetooth beacon scanner program

--- services/flash_periodic_restart.sh
+++ services/flash_periodic_restart.sh
@@ -2,27 +2,44 @@
 
 export famId=123XXX
 export usrName=flashsysXXX
-export BACKUP_DIRS="/home/${usrName}/data /home/${usrName}/docker-compose/ha-config"
+export logFolder="/home/${usrName}/data/${famId}_data/logs"
+
+source "/home/${usrName}/py38/bin/activate"
 
 #logFolderPath=/home/$usrName/data/${famId}_data
-logFolder=/home/$usrName/data/${famId}_data/logs
-mkdir -p $logFolder
+mkdir -p ${logFolder}
 
-reboot_index_path="/home/$usrName/data/${famId}_data/${famId}_reboot_index.txt"
+python3 "/home/${usrName}/flash-tv-scripts/python_scripts/check_file_events.py" ${famId} ${logFolder} "/home/${usrName}/data/${famId}_data/${famId}_varlog_filesequence.csv" &
 
-tegrastats --interval 30000 --logfile /home/${usrName}/data/${famId}_data/${famId}_tegrastats.log &
-bash /home/${usrName}/flash-tv-scripts/services/flash_check_camera_warnings.sh ${famId} ${usrName} &
+python3 "/home/${usrName}/flash-tv-scripts/python_scripts/check_file_events.py" ${famId} "/home/${usrName}/data/${famId}_data /home/${usrName}/data/${famId}_data/${famId}_flashlog_filesequence.csv" &
 
-source /home/$usrName/py38/bin/activate
-python /home/${usrName}/flash-tv-scripts/python_scripts/check_file_events.py $famId $logFolder /home/${usrName}/data/${famId}_data/${famId}_varlog_filesequence.csv &
+tegrastats --interval 30000 --logfile "/home/${usrName}/data/${famId}_data/${famId}_tegrastats.log" &
+
+bash "/home/${usrName}/flash-tv-scripts/services/flash_check_camera_warnings.sh" ${famId} ${usrName} &
+
+BEACON_SCANNER_PROGRAM_PATH="/home/${usrName}/flash-tv-scripts/services/bt_beacon_accelerometer_scanner"
+
+if [ -e ${BEACON_SCANNNER_PROGRAM_PATH} ]; then
+
+    bash "/home/${usrName}/flash-tv-scripts/services/run_beacon_scanner.sh" ${BEACON_SCANNNER_PROGRAM_PATH} &
+
+else
+
+    cc "${BEACON_SCANNNER_PROGRAM_PATH}.c" -lbluetooth -o ${BEACON_SCANNER_PROGRAM_PATH}
+    
+    bash "/home/${usrName}/flash-tv-scripts/services/run_beacon_scanner.sh" ${BEACON_SCANNNER_PROGRAM_PATH} &
+
+fi
 
 sleep 10;
 
-if [ -e "$reboot_index_path" ]; then
+REBOOT_INDEX_PATH="/home/${usrName}/data/${famId}_data/${famId}_reboot_index.txt"
 
-    last_line=$(tail -n 1 "$reboot_index_path")
+if [ -e ${REBOOT_INDEX_PATH} ]; then
+
+    last_line=$(tail -n 1 ${REBOOT_INDEX_PATH})
     
-    last_index=$(echo "$last_line" | awk '{print $NF}')
+    last_index=$(echo ${last_line} | awk '{print $NF}')
     
     new_index=$((last_index + 1))
     
@@ -34,10 +51,9 @@ fi
 
 dt_for_index=$(date +"%d_%b_%Y_%H-%M-%S_%Z")
 
-echo "flash_periodic_restart.sh was just restarted around ${dt_for_index}, implying that the current reboot index is: ${new_index}" >> "$reboot_index_path"
+echo "flash_periodic_restart.sh was just restarted around ${dt_for_index}, implying that the current reboot index is: ${new_index}" >> ${reboot_index_path}
 
 loop=1
-
 while true;
 do
 	sleep 21600;
@@ -53,13 +69,13 @@ do
 	echo "Reboot Index: ${new_index}" >> "${logFolder}/varlogs_${dt}/logend_${dt}.txt"
 	systemctl status flash-run-on-boot.service >> "${logFolder}/varlogs_${dt}/logend_${dt}.txt"
 	echo "Reboot Index: ${new_index}" >> "${logFolder}/varlogs_${dt}/timedate_${dt}.txt"
-	python3 /home/$usrName/flash-tv-scripts/python_scripts/check_all_times.py >> "${logFolder}/varlogs_${dt}/timedate_${dt}.txt"
+	python3 /home/${usrName}/flash-tv-scripts/python_scripts/check_all_times.py >> "${logFolder}/varlogs_${dt}/timedate_${dt}.txt"
 	v4l2-ctl --list-devices > "${logFolder}/varlogs_${dt}/camera_${dt}.txt"
 	
 	pkill -9 -f run_flash_data_collection.py
 	
-	mv /home/$usrName/data/${famId}_data/${famId}_flash_logstdout.log /home/$usrName/data/${famId}_data/${famId}_flash_logstderr.log "${logFolder}/varlogs_${dt}"
-	cp /home/$usrName/data/${famId}_data/${famId}_flash_logstdoutp.log /home/$usrName/data/${famId}_data/${famId}_flash_logstderrp.log "${logFolder}/varlogs_${dt}"
+	mv "/home/${usrName}/data/${famId}_data/${famId}_flash_logstdout.log" "/home/${usrName}/data/${famId}_data/${famId}_flash_logstderr.log" "${logFolder}/varlogs_${dt}"
+	cp "/home/${usrName}/data/${famId}_data/${famId}_flash_logstdoutp.log" "/home/${usrName}/data/${famId}_data/${famId}_flash_logstderrp.log" "${logFolder}/varlogs_${dt}"
 	#mv /var/log/"${famId}_flash_logstdout.log" /var/log/"${famId}_flash_logstderr.log" "${logFolder}/varlogs_${dt}"
 	#cp /var/log/"${famId}_flash_logstdoutp.log" /var/log/"${famId}_flash_logstderrp.log" "${logFolder}/varlogs_${dt}"
  
@@ -67,7 +83,7 @@ do
 
 		if [ `lsblk -o NAME,TRAN,MOUNTPOINT | grep -A 1 -w usb | grep -v usb | awk '{print $2}'` ]; then
 	 
-	 		backup_usb_path=`lsblk -o NAME,TRAN,MOUNTPOINT | grep -A 1 -w usb | grep -v usb | awk '{print $2}'`
+	 		BACKUP_USB_PATH=`lsblk -o NAME,TRAN,MOUNTPOINT | grep -A 1 -w usb | grep -v usb | awk '{print $2}'`
 
     		else
       			echo "Backup USB not Found in lsblk at Time: ${dt}"
@@ -75,12 +91,14 @@ do
 	 	fi
 
  		source "/home/${usrName}/.bashrc"
+
+  		export BACKUP_DIRS="/home/${usrName}/data /home/${usrName}/docker-compose/ha-config"
 	
 		borg create --exclude "/home/${usrName}/data/*.zip" --exclude "/home/${usrName}/data/*/*face*" ::${famId}-FLASH-HA-Data-Backup-${dt} ${BACKUP_DIRS}
 		
 		echo "USB Backup without Face Folders Created at Time: ${dt}"
 
-  		source /home/$usrName/py38/bin/activate
+  		source "/home/${usrName}/py38/bin/activate"
 			
 	else
 		
