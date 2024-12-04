7d7825396d7b509f6b11e7514830dbd4cf794795
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2024-12-04 16:54:10 -0600

    modified create_faces.sh to allow for optional selection of an extra face and fixed some wording

--- runtime_scripts/create_faces.sh
+++ runtime_scripts/create_faces.sh
@@ -7,7 +7,7 @@ user_resp=$?
 if [ $user_resp -eq 1 ]; then
 	#echo "Exitted the code $user_resp"
 	zenity --warning --text="Exiting the code since data details are not correct. Please modify them and restart the script."
-	exit 
+	exit 1
 fi
 
 mkdir -p $savePath/"${famId}_faces"
@@ -15,16 +15,16 @@ mkdir -p $savePath/"${famId}_faces"
 
 if [ ! -d $savePath/"${famId}_face_crops" ]
 then
-	zenity --warning --title "Warning Message" --width 700 --height 100 --text "The face crops directory indicated $savePath/${famId}_face_crops does not exist. \nPlease check data save path if the face crops directory is present. \nPlease restart with right data save path ";
-	exit 
+	zenity --warning --title "Warning Message" --width 700 --height 100 --text "The indicated face_crops directory $savePath/${famId}_face_crops does not exist. \nPlease check if the face_crops directory is present.";
+	exit 1
 fi
 
 # target child processing
 ntc=`ls $savePath/"${famId}_face_crops"/tc_selected/*.png | wc -l`
 min=5
 if [ $ntc -lt $min ]; then
-	zenity --warning --title "Warning Message" --width 700 --height 100 --text "The Target child faces selected for gallery are less than $min. \nPlease check folder $savePath/${famId}_face_crops/tc_selected if it has 5 faces. \nPlease restart with right data save path "
-	exit
+	zenity --warning --title "Warning Message" --width 700 --height 100 --text "The number of target child faces selected for the gallery is less than $min. \nPlease check if the folder $savePath/${famId}_face_crops/tc_selected has less than $min faces."
+ 	exit 1
 fi
 
 
@@ -39,8 +39,8 @@ done
 nsib=`ls $savePath/"${famId}_face_crops"/sib_selected/*.png | wc -l`
 min=5
 if [ $nsib -lt $min ]; then
-	zenity --warning --title "Warning Message" --width 700 --height 100 --text "The Sibling faces selected for gallery are less than $min. \nPlease check folder $savePath/${famId}_face_crops/sib_selected if it has 5 faces. \nPlease restart with right data save path "
-	exit
+	zenity --warning --title "Warning Message" --width 700 --height 100 --text "The number of sibling faces selected for the gallery is less than $min. \nPlease check if the folder $savePath/${famId}_face_crops/sib_selected has less than $min faces."
+ 	exit 1
 fi
 
 
@@ -57,8 +57,8 @@ done
 npar=`ls $savePath/"${famId}_face_crops"/par_selected/*.png | wc -l`
 min=5
 if [ $npar -lt $min ]; then
-	zenity --warning --title "Warning Message" --width 700 --height 100 --text "The Parent faces selected for gallery are less than $min. \nPlease check folder $savePath/${famId}_face_crops/par_selected if it has 5 faces. \nPlease restart with right data save path "
-	exit
+	zenity --warning --title "Warning Message" --width 700 --height 100 --text "The number of parent faces selected for the gallery is less than $min. \nPlease check if the folder $savePath/${famId}_face_crops/par_selected has less than $min faces."
+ 	exit 1
 fi
 
 # parent processing
@@ -70,11 +70,26 @@ do
 	cp $i $savePath/"${famId}_faces"/"${famId}_parent${n}.png"
 done
 
-# poster processing
+nextra=`ls $savePath/"${famId}_face_crops"/extra_selected/*.png | wc -l`
+min=5
+if [ $nextra -lt $min ]; then
+	zenity --warning --title "Warning Message" --width 700 --height 100 --text "The extra faces selected for gallery is less than $min. \nPlease check if the folder $savePath/${famId}_face_crops/extra_selected has less than $min faces."
+	exit
+fi
+
+# extra processing
 n=0
-for i in ../poster_faces/*.png;
-do 
-	#echo $i;
-	n=$((n+1))
-	cp $i $savePath/"${famId}_faces"/"${famId}_poster${n}.png"
-done
+extra_images=($savePath/"${famId}_face_crops"/extra_selected/*.png)
+if [ -e "${extra_images[0]}" ]; then
+    for i in "${extra_images[@]}"; do
+        n=$((n+1))
+        cp "$i" "$savePath/${famId}_faces/${famId}_extra${n}.png"
+    done
+else
+    # poster processing
+    n=0
+    for i in ../poster_faces/*.png; do
+        n=$((n+1))
+        cp "$i" "$savePath/${famId}_faces/${famId}_extra${n}.png"
+    done
+fi
