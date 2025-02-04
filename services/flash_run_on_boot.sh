e92ca0bc293c19df4dc06cddb41e6489fa330b37
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2025-02-04 12:27:10 -0600

    comments

--- services/flash_run_on_boot.sh
+++ services/flash_run_on_boot.sh
@@ -8,20 +8,22 @@ export PATH=/usr/local/cuda-11/bin:$PATH
 export MXNET_HOME=/home/$username/mxnet
 export PYTHONPATH=$MXNET_HOME/python:$PYTHONPATH
 
-source /home/${username}/py38/bin/activate
+# Activate Python 3.8 virtual environment with libraries set up
+source "/home/${username}/py38/bin/activate"
 
+# Disable automatic time updating and update the time from the RTCs instead
 timedatectl set-ntp 0;
 sleep 1;
 python3 /home/${username}/flash-tv-scripts/python_scripts/update_system_time_from_RTCs.py /home/${username}/data/${participantID}_data/${participantID}_start_date.txt
 
+# Create a local network (without internet) to connect to when transferring data at visit 2
 suffix=$(printf "%d" "${username: -2}")
 
 if ! sudo ip addr add 10.0.0."${suffix}"/24 dev eth0 2>&1 | grep -q 'RTNETLINK answers: File exists'; then
     echo "Info: IP was possibly already assigned in this reboot cycle as the 'RTNETLINK answers: File exists' message was detected."
 fi
 
-# runs a while loop for the FLASH-TV algorithm only if it doesn't already exist
-
+# Run a while loop for the FLASH-TV algorithm only if it doesn't already exist
 while true;
 do	
 if ! [ "$(pgrep -af run_flash_data_collection.py)" ]; then
