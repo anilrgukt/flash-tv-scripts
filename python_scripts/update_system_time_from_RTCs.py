422033ff5e89c1067d1207d8cee5705795df1c82
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2025-02-04 12:30:38 -0600

    renaming and refactoring

--- python_scripts/update_system_time_from_RTCs.py
+++ python_scripts/update_system_time_from_RTCs.py
@@ -42,50 +42,58 @@ def reboot_sequence(max_retries: int, intervals: list[int]) -> None:
         reboot(interval, max_retries)
 
 
-def read_RTC_date(bus: SMBus) -> list[int]:
+def read_RTC_datetime(bus: SMBus) -> list[int]:
     return bus.read_i2c_block_data(RTC_ADDRESS, 0, 8)
 
 
-def hex_RTC_date(bus: SMBus) -> list[str]:
-    return [hex(x) for x in read_RTC_date(bus=bus)]
+def convert_external_RTC_datetime_to_hexadecimal_format(bus: SMBus) -> list[str]:
+    return [hex(x) for x in read_RTC_datetime(bus=bus)]
 
 
-def dec_RTC_date(bus: SMBus) -> list[int]:
-    return [int(x.replace("0x", "")) for x in hex_RTC_date(bus=bus)]
+def convert_external_RTC_datetime_to_decimal_format(bus: SMBus) -> list[int]:
+    return [int(x.replace("0x", "")) for x in convert_external_RTC_datetime_to_hexadecimal_format(bus=bus)]
 
 
-def is_within_12_days(*, file_path: Path | str, ext_RTC_date: datetime_class | None = None) -> bool | None:
-    if Path(file_path).exists():
-        with Path(file_path).open() as file:
-            start_date_str = file.read().strip()
+def get_start_datetime() -> datetime_class | None:
+    start_datetime_file_path = Path(sys.argv[1])
 
-        start_date = datetime_class.strptime(start_date_str, "%Y-%m-%d %H:%M:%S")
-
-        if ext_RTC_date:
-            delta = abs(ext_RTC_date - start_date)
-
-            if delta.days <= 12:
-                return True
-            else:
-                stderr_print("The date from the external RTC was more than 12 days away from the start date")
-                return False
-        else:
-            stderr_print("No external RTC date provided for comparison to the start date")
-            return None
+    if Path(start_datetime_file_path).exists():
+        with Path(start_datetime_file_path).open() as file:
+            start_datetime_str = file.read().strip()
+            return datetime_class.strptime(start_datetime_str, "%Y-%m-%d %H:%M:%S")
     else:
-        stderr_print(f"Start date file '{file_path}' not found")
+        stderr_print(f"Start datetime file '{start_datetime_file_path}' not found")
         return None
 
 
-def convert_RTC_format_to_timedatectl_format(bus: SMBus) -> str:
-    try:
-        RTC_date = dec_RTC_date(bus=bus)
-        formatted_RTC_date = f"20{RTC_date[6]:02}-{RTC_date[5]:02}-{RTC_date[4]:02} {RTC_date[2]:02}:{RTC_date[1]:02}:{RTC_date[0]:02}"
-        datetime_RTC_date = datetime_class.strptime(formatted_RTC_date, "%Y-%m-%d %H:%M:%S")
-        if is_within_12_days(file_path=START_DATE_FILE_PATH, ext_RTC_date=datetime_RTC_date):
-            return formatted_RTC_date
+def is_external_RTC_datetime_within_12_days_of_start_datetime(*, RTC_datetime: datetime_class | None = None) -> bool | None:
+    if RTC_datetime:
+        start_datetime = get_start_datetime()
+        if start_datetime:
+            datetime_difference = abs(RTC_datetime - start_datetime)
+            if datetime_difference.days <= 12:
+                return True
+            else:
+                stderr_print("The datetime from the external RTC was more than 12 days away from the start datetime")
+                return False
         else:
-            return f"The date from the external RTC, 20{RTC_date[6]:02}-{RTC_date[5]:02}-{RTC_date[4]:02} {RTC_date[2]:02}:{RTC_date[1]:02}:{RTC_date[0]:02}, was incomparable or incorrect"
+            stderr_print("No start datetime provided for comparison to the external RTC datetime")
+            return None
+    else:
+        stderr_print("No external RTC datetime provided for comparison to the start datetime")
+        return None
+
+
+def convert_external_RTC_datetime_format_to_timedatectl_format(bus: SMBus) -> str:
+    try:
+        RTC_datetime_decimal_array = convert_external_RTC_datetime_to_decimal_format(bus=bus)
+        RTC_datetime_str = f"20{RTC_datetime_decimal_array[6]:02}-{RTC_datetime_decimal_array[5]:02}-{RTC_datetime_decimal_array[4]:02} {RTC_datetime_decimal_array[2]:02}:{RTC_datetime_decimal_array[1]:02}:{RTC_datetime_decimal_array[0]:02}"
+        RTC_datetime = datetime_class.strptime(RTC_datetime_str, "%Y-%m-%d %H:%M:%S")
+
+        if is_external_RTC_datetime_within_12_days_of_start_datetime(RTC_datetime=RTC_datetime):
+            return RTC_datetime_str
+        else:
+            return f"The datetime from the external RTC, 20{RTC_datetime_decimal_array[6]:02}-{RTC_datetime_decimal_array[5]:02}-{RTC_datetime_decimal_array[4]:02} {RTC_datetime_decimal_array[2]:02}:{RTC_datetime_decimal_array[1]:02}:{RTC_datetime_decimal_array[0]:02}, was incomparable or incorrect"
     except Exception as e:
         return str(e)
 
@@ -112,44 +120,49 @@ def run_command_and_raise_exceptions(command: list[str], error_message: str, suc
         raise
 
 
-def check_times(bus: SMBus) -> None:
+def check_all_datetimes() -> None:
     print(run_command_and_ignore_exceptions(["timedatectl"], "Unable to run timedatectl for system time info"))
 
     print(
         f"Time from internal RTC rtc0 (PSEQ_RTC, being used) is: {run_command_and_ignore_exceptions(['sudo', 'hwclock', '-r'], 'Unable to obtain time from internal RTC rtc0 (PSEQ_RTC, being used) for validation')}"
     )
 
-    print(f"Time from external RTC (DS3231) is: {convert_RTC_format_to_timedatectl_format(bus=bus)}")
+    bus = SMBus(I2C_BUS_NUMBER)
+    print(f"Time from external RTC (DS3231) is: {convert_external_RTC_datetime_format_to_timedatectl_format(bus=bus)}")
+    if bus:
+        bus.close()
 
     print(
         f"Time from internal RTC rtc1 (tegra-RTC, not being used) is: {run_command_and_ignore_exceptions(['sudo', 'hwclock', '--rtc', '/dev/rtc1'], 'Unable to obtain time from internal RTC rtc1 (tegra-RTC, not being used)')}"
     )
 
 
-def set_time_external(bus: SMBus) -> None:
+def set_datetime_from_external_rtc(bus: SMBus) -> None:
     success_message = "The system time was set from the external RTC"
-    command = ["sudo", "timedatectl", "set-time", convert_RTC_format_to_timedatectl_format(bus=bus)]
+    command = ["sudo", "timedatectl", "set-time", convert_external_RTC_datetime_format_to_timedatectl_format(bus=bus)]
     run_command_and_raise_exceptions(command, "Failed to set time from external RTC", success_message)
 
 
-def set_time_internal() -> None:
+def set_datetime_from_internal_rtc() -> None:
     success_message = "The system time was set from the internal RTC"
     command = ["sudo", "hwclock", "-s"]
     run_command_and_raise_exceptions(command, "Failed to set time from internal RTC", success_message)
 
 
-def set_time_both(bus: SMBus) -> None:
+def set_datetime_on_both_RTCs() -> None:
+    bus = None
     try:
-        set_time_external(bus=bus)
+        bus = SMBus(I2C_BUS_NUMBER)
+        set_datetime_from_external_rtc(bus=bus)
     except Exception:
-        set_time_internal()
+        set_datetime_from_internal_rtc()
+    finally:
+        if bus:
+            bus.close()
 
 
-def set_time(bus: SMBus) -> None:
-    def set_time_both_partial(bus: SMBus = bus):
-        return set_time_both(bus=bus)
-
-    if retry_function(set_time_both_partial, MAX_RETRIES, 1, "Failed to set time from both RTC sources"):
+def set_datetime_with_retries() -> None:
+    if retry_function(set_datetime_on_both_RTCs, MAX_RETRIES, 1, "Failed to set time from both RTC sources"):
         return
     else:
         reboot_sequence(MAX_RETRIES, [1, 60, 300])
@@ -158,14 +171,9 @@ def set_time(bus: SMBus) -> None:
 if __name__ == "__main__":
     # Constants
     MAX_RETRIES = 60
-    RTC_ADDRESS = 104
+    RTC_ADDRESS = 104  # Replace with the actual RTC address if different
     I2C_BUS_NUMBER = 1  # Replace with the actual bus number if different
-    BUS = SMBus(I2C_BUS_NUMBER)
-    START_DATE_FILE_PATH = Path(sys.argv[1])
-    # print(START_DATE_FILE_PATH)
 
-    set_time(bus=BUS)
-    check_times(bus=BUS)
+    set_datetime_with_retries()
 
-    if BUS:
-        BUS.close()
+    check_all_datetimes()
