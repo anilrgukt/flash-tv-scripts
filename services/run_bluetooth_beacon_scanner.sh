ed3216991356d7115ac7e8b61b96f03586fba955
Rohitaa Ravikumar <113260877+Rohitaa1306@users.noreply.github.com>
2025-02-12 14:26:28 -0600

    Update run_bluetooth_beacon_scanner.sh

--- services/run_bluetooth_beacon_scanner.sh
+++ services/run_bluetooth_beacon_scanner.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-BLUETOOTH_BEACON_SCANNER_PROGRAM_PATH= "/home/flash-tv-scripts/services/bluetooth_beacon_accelerometer_data_reader"
+BLUETOOTH_BEACON_SCANNER_PROGRAM_PATH="/home/${username}/flash-tv-scripts/services/bluetooth_beacon_accelerometer_data_reader"
 
 if [ ! -e "${BLUETOOTH_BEACON_SCANNER_PROGRAM_PATH}" ]; then
-
-    cc "${BLUETOOTH_BEACON_SCANNER_PROGRAM_PATH}.c" -lbluetooth -o ${BLUETOOTH_BEACON_SCANNER_PROGRAM_PATH}
-    
+    cc "${BLUETOOTH_BEACON_SCANNER_PROGRAM_PATH}.c" -lbluetooth -o "${BLUETOOTH_BEACON_SCANNER_PROGRAM_PATH}"
 fi
 
 while true; do
