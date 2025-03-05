e9f83c7a860deba5ccea07cb20107ac2fc51d384
uzairbcm <124936575+uzairbcm@users.noreply.github.com>
2025-03-05 10:20:43 -0600

    rename fix

--- setup_scripts/USB_backup_setup.sh
+++ setup_scripts/USB_backup_setup.sh
@@ -41,7 +41,7 @@ if lsusb | grep -q "SanDisk Corp. Ultra Fit"; then
 	zenity --entry --hide-text --width 500 --height 100 --text="Enter USB Backup Password:" > "${temp_file}"
 	
 	# Send password to be checked and encoded
-	encoded_password=$(python3 "/home/${username}/flash-tv-scripts/python_scripts/check_and_encode_password.py" "${temp_file}")
+	encoded_password=$(python3 "/home/${username}/flash-tv-scripts/python_scripts/check_and_encode_passphrase.py" "${temp_file}")
 	exit_code=$?
 	
 	# Overwrite and destroy temp file
