8aaa1ea3dfb7417fd5b6fea95b34e4cf6640a300
uzaira0 <uzair.alam@bcm.edu>
2025-09-26 17:54:51 -0500

    typopo

--- gui_second_version/steps/__init__.py
+++ gui_second_version/steps/__init__.py
@@ -10,11 +10,9 @@ from steps.gaze_detection_testing_step import GazeDetectionTestingStep
 from steps.log_file_verification_step import LogFileVerificationStep
 from steps.participant_setup_step import ParticipantSetupStep
 from steps.pov_picture_step import POVPictureStep
-from steps.service_configuration_step import ServiceConfigurationStep
 from steps.smart_plug_physical_step import SmartPlugPhysicalStep
 from steps.smart_plug_verify_step import SmartPlugVerifyStep
 from steps.step_factory import StepFactory
-from steps.system_testing_step import SystemTestingStep
 from steps.time_sync_step import TimeSyncStep
 from steps.wifi_connection_step import WiFiConnectionStep
 
@@ -27,11 +25,9 @@ __all__ = [
     "LogFileVerificationStep",
     "POVPictureStep",
     "ParticipantSetupStep",
-    "ServiceConfigurationStep",
     "SmartPlugPhysicalStep",
     "SmartPlugVerifyStep",
     "StepFactory",
-    "SystemTestingStep",
     "TimeSyncStep",
     "WiFiConnectionStep",
 ]
